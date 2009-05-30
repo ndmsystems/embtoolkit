4c23ae5a7544945a1e262f326c3f40c65607b2e0
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-05-30 20:30:56 +0200

    Packages: add packages Makefile
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

---
+++ mk/packages.mk
@@ -0,0 +1,27 @@
+#########################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+#########################################################################################
+#
+# \file         packages.mk
+# \brief	packages.mk of Embtoolkit
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         May 2009
+#########################################################################################
+
+#Busybox
+include $(EMBTK_ROOT)/packages/busybox/busybox.mk
+
