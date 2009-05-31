2bc7fbe433cbd8d06d71bcd19ff2fdab87ec2212
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-05-31 22:58:05 +0200

    Packages: make busybox buildable
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- packages/busybox/busybox.mk
+++ packages/busybox/busybox.mk
@@ -23,7 +23,26 @@
 #########################################################################################
 
 BB_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_BB_VERSION_STRING)))
+BB_DOT_CONFIG := $(subst ",,$(strip $(CONFIG_EMBTK_BB_DOT_CONFIG)))
 BB_SITE := http://www.busybox.net/downloads
 BB_PACKAGE := busybox-$(BB_VERSION).tar.bz2
-BB_BUILD_DIR := $(TOOLS_BUILD)/busybox
+BB_BUILD_DIR := $(PACKAGES_BUILD)/busybox-$(BB_VERSION)
 
+busybox_install: $(BB_BUILD_DIR)/.installed
+
+$(BB_BUILD_DIR)/.installed: download_busybox $(BB_BUILD_DIR)/.decompressed
+	CFLAGS="-Os -pipe -fno-strict-aliasing" \
+	$(MAKE) -C $(BB_BUILD_DIR) CROSS_COMPILE=$(TOOLS)/bin/$(STRICT_GNU_TARGET)- \
+	CONFIG_PREFIX=$(ROOTFS) install
+	touch $@
+
+download_busybox:
+	@test -e $(DOWNLOAD_DIR)/$(BB_PACKAGE) || \
+	wget -O $(DOWNLOAD_DIR)/$(BB_PACKAGE) $(BB_SITE)/$(BB_PACKAGE)
+
+$(BB_BUILD_DIR)/.decompressed:
+	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(BB_PACKAGE) ...")
+	@tar -C $(PACKAGES_BUILD) -xjf $(DOWNLOAD_DIR)/$(BB_PACKAGE)
+	@test -e $(BB_BUILD_DIR)/.config || \
+	cp $(EMBTK_ROOT)/packages/busybox/$(BB_DOT_CONFIG) $(BB_BUILD_DIR)/.config
+	@touch $@
