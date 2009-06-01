349ee2b8020683376344205dd5fe71c45aec03f0
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-06-01 11:14:52 +0200

    Rootfs: Add fakeroot tool
    This patch adds fakeroot tool.
    fakeroot helps building root filesystem, without the need to be root.
    
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

---
+++ mk/fakeroot.mk
@@ -0,0 +1,53 @@
+#########################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+#########################################################################################
+#
+# \file         fakeroot.mk
+# \brief	fakeroot.mk of Embtoolkit. fakeroot helps building root filesystem,
+# \brief	without the need to be root.
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         June 2009
+#########################################################################################
+
+FAKEROOT_VERSION := 1.11
+FAKEROOT_SITE := http://snapshot.debian.net/archive/2008/12/31/debian/pool/main/f/fakeroot
+FAKEROOT_PACKAGE := fakeroot_$(FAKEROOT_VERSION).tar.gz
+FAKEROOT_BUILD_DIR := $(TOOLS_BUILD)/fakeroot-build
+FAKEROOT_DIR := $(EMBTK_ROOT)/fakeroot
+
+fakeroot_install: $(FAKEROOT_BUILD_DIR)/.installed
+
+$(FAKEROOT_BUILD_DIR)/.installed: download_fakeroot $(FAKEROOT_BUILD_DIR)/.decompressed \
+	$(FAKEROOT_BUILD_DIR)/.configured
+	@$(MAKE) -C $(FAKEROOT_BUILD_DIR) && $(MAKE) -C $(FAKEROOT_BUILD_DIR) install
+	@touch $@
+
+download_fakeroot:
+	@test -e $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE) || \
+	wget -O $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE) $(FAKEROOT_SITE)/$(FAKEROOT_PACKAGE)
+
+$(FAKEROOT_BUILD_DIR)/.decompressed:
+	@tar -C $(TOOLS_BUILD) -xzf $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE)
+	@mkdir -p $(FAKEROOT_BUILD_DIR)
+	@mkdir -p $(FAKEROOT_DIR)
+	@touch $@
+
+$(FAKEROOT_BUILD_DIR)/.configured:
+	@cd $(FAKEROOT_BUILD_DIR); \
+	$(TOOLS_BUILD)/fakeroot-$(FAKEROOT_VERSION)/configure \
+	--prefix=$(FAKEROOT_DIR) --build=$(HOST_BUILD) --host=$(HOST_ARCH)
+	@touch $@
