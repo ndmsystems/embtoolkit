83820129d57a2f475dd223a7eebc9c5343f1c740
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-06-07 22:26:18 +0200

    Toolchain: Rewrite egligc options parse
    This patch rewrites the way we parse eglibc options. It creates two new files
    in order to separate eglibc-2.9 and eglibc-2.10 options.
    
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

---
+++ mk/eglibc-2.9-options.mk
@@ -0,0 +1,35 @@
+#########################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+#########################################################################################
+#
+# \file         eglibc-2.9-options.mk
+# \brief	eglibc-2.9-options.mk. Here we define options supported by eglibc-2.9
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         May 2009
+#########################################################################################
+
+EGLIBC_OPTIONS := OPTION_EGLIBC_ADVANCED_INET6 OPTION_EGLIBC_BACKTRACE OPTION_EGLIBC_BSD \
+		OPTION_EGLIBC_BSD OPTION_EGLIBC_CXX_TESTS OPTION_EGLIBC_CATGETS \
+		OPTION_EGLIBC_CHARSETS OPTION_EGLIBC_DB_ALIASES OPTION_EGLIBC_ENVZ \
+		OPTION_EGLIBC_FSTAB OPTION_EGLIBC_GETLOGIN OPTION_EGLIBC_INET \
+		OPTION_EGLIBC_LIBM OPTION_EGLIBC_LOCALES OPTION_EGLIBC_LOCALE_CODE \
+		OPTION_EGLIBC_NIS OPTION_EGLIBC_NSSWITCH OPTION_EGLIBC_RCMD \
+		OPTION_EGLIBC_SPAWN OPTION_EGLIBC_STREAMS OPTION_EGLIBC_SUNRPC \
+		OPTION_EGLIBC_UTMP OPTION_EGLIBC_UTMPX OPTION_EGLIBC_WORDEXP \
+		OPTION_POSIX_C_LANG_WIDE_CHAR OPTION_POSIX_REGEXP \
+		OPTION_POSIX_WIDE_CHAR_DEVICE_IO
+
