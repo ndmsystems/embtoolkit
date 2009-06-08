5a9e798718bd8a07565ea885a41eef3719de0599
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-06-08 21:35:21 +0200

    Toolchain: fix successful toolchain build message
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- mk/toolchain.mk
+++ mk/toolchain.mk
@@ -53,7 +53,7 @@ buildtoolchain: mkinitialpath ccachehost_install gmphost_install \
 		mpfrhost_install binutils_install gcc1_install \
 		kernel-headers_install eglibc-headers_install \
 		gcc2_install eglibc_install gcc3_install symlink_tools
-	$(call EMBTK_GENERIC_MESSAGE,"You successfully build a toolchain for $(STRICT_GNU_TARGET !!!")
+	$(call EMBTK_GENERIC_MESSAGE,"You successfully build a toolchain for $(STRICT_GNU_TARGET) !!!")
 symlink_tools:
 	@cd $(TOOLS)/bin/; export TOOLS_LIST="`ls`"; \
 	for i in $$TOOLS_LIST;do \
