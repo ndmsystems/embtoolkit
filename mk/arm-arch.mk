2e53850f2b1da81c0971cb2a94142d75d78b0988
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-08-18 12:01:55 +0200

    Toolchain: arm: check if multilib is selected or not
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- mk/arm-arch.mk
+++ mk/arm-arch.mk
@@ -25,6 +25,7 @@
 ifeq ($(CONFIG_EMBTK_ARCH_ARM),y)
 LINUX_ARCH := arm
 GNU_TARGET_ARCH := arm
+EMBTK_MCU_FLAG := $(subst ",,$(strip $(CONFIG_EMBTK_ARM_MCU_STRING)))
 
 ifeq ($(CONFIG_EMBTK_ARCH_ARM_BIG_ENDIAN),y)
 GNU_TARGET := armeb-linux
@@ -36,7 +37,11 @@ endif
 
 #GCC configure options
 GCC_WITH_CPU := --with-cpu=$(subst ",,$(strip $(CONFIG_EMBTK_ARM_MCU_STRING)))
-EMBTK_MCU_FLAG := $(subst ",,$(strip $(CONFIG_EMBTK_ARM_MCU_STRING)))
+ifeq ($(CONFIG_EMBTK_TOOLCHAIN_MULTILIB),)
+GCC_MULTILIB := --disable-multilib
+else
+GCC_MULTILIB :=
+endif
 
 #GCC extra configure options for arm
 ifeq ($(CONFIG_EMBTK_GCC_LANGUAGE_JAVA),y)
