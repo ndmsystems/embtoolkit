bf2eb832bcde4a2e2a99fc35d7bf21d87429c6b9
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2009-11-11 13:45:13 +0100

    Toolchain: eglibc: consolidate eglibc option groups
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- mk/eglibc-2.10-options.mk
+++ mk/eglibc-2.10-options.mk
@@ -1,4 +1,4 @@
-#########################################################################################
+################################################################################
 # GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
 # Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
 #
@@ -14,23 +14,43 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
-#########################################################################################
+################################################################################
 #
 # \file         eglibc-2.10-options.mk
-# \brief	eglibc-2.10-options.mk. Here we define options supported by eglibc-2.9
+# \brief	eglibc-2.10-options.mk. Here we define options supported by
+# \brief	eglibc-2.10
 # \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
 # \date         May 2009
-#########################################################################################
+################################################################################
 
-EGLIBC_OPTIONS := OPTION_EGLIBC_ADVANCED_INET6 OPTION_EGLIBC_BACKTRACE OPTION_EGLIBC_BSD \
-		OPTION_EGLIBC_CXX_TESTS OPTION_EGLIBC_CATGETS OPTION_EGLIBC_CHARSETS \
-		OPTION_EGLIBC_DB_ALIASES OPTION_EGLIBC_ENVZ OPTION_EGLIBC_FCVT \
-		OPTION_EGLIBC_FMTMSG OPTION_EGLIBC_FSTAB OPTION_EGLIBC_FTRAVERSE \
-		OPTION_EGLIBC_GETLOGIN OPTION_EGLIBC_INET OPTION_EGLIBC_LIBM \
-		OPTION_EGLIBC_LOCALES OPTION_EGLIBC_LOCALE_CODE OPTION_EGLIBC_NIS \
-		OPTION_EGLIBC_NSSWITCH OPTION_EGLIBC_RCMD OPTION_EGLIBC_SPAWN \
-		OPTION_EGLIBC_STREAMS OPTION_EGLIBC_SUNRPC OPTION_EGLIBC_UTMP \
-		OPTION_EGLIBC_UTMPX OPTION_EGLIBC_WORDEXP OPTION_POSIX_C_LANG_WIDE_CHAR \
-		OPTION_POSIX_REGEXP OPTION_POSIX_REGEXP_GLIBC \
+EGLIBC_OPTIONS := OPTION_EGLIBC_ADVANCED_INET6		\
+		OPTION_EGLIBC_BACKTRACE			\
+		OPTION_EGLIBC_BSD			\
+		OPTION_EGLIBC_CXX_TESTS			\
+		OPTION_EGLIBC_CATGETS			\
+		OPTION_EGLIBC_CHARSETS			\
+		OPTION_EGLIBC_DB_ALIASES		\
+		OPTION_EGLIBC_ENVZ			\
+		OPTION_EGLIBC_FCVT			\
+		OPTION_EGLIBC_FMTMSG			\
+		OPTION_EGLIBC_FSTAB			\
+		OPTION_EGLIBC_FTRAVERSE			\
+		OPTION_EGLIBC_GETLOGIN			\
+		OPTION_EGLIBC_INET			\
+		OPTION_EGLIBC_LIBM			\
+		OPTION_EGLIBC_LOCALES			\
+		OPTION_EGLIBC_LOCALE_CODE		\
+		OPTION_EGLIBC_NIS			\
+		OPTION_EGLIBC_NSSWITCH			\
+		OPTION_EGLIBC_RCMD			\
+		OPTION_EGLIBC_SPAWN			\
+		OPTION_EGLIBC_STREAMS			\
+		OPTION_EGLIBC_SUNRPC			\
+		OPTION_EGLIBC_UTMP			\
+		OPTION_EGLIBC_UTMPX			\
+		OPTION_EGLIBC_WORDEXP			\
+		OPTION_POSIX_C_LANG_WIDE_CHAR		\
+		OPTION_POSIX_REGEXP			\
+		OPTION_POSIX_REGEXP_GLIBC		\
 		OPTION_POSIX_WIDE_CHAR_DEVICE_IO
 
