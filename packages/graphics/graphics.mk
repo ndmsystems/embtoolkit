ndmsystems/embtoolkit

commit 95b7b617b53eb2b6a33fa4cd2d9ed3c404d2c220

Packages: Rework build process and use foo-y targets
Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

packages/graphics/graphics.mk
@@ -1,0 +1,101 @@
+################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+################################################################################
+#
+# \file         graphics.mk
+# \brief	graphics.mk of Embtoolkit
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         February 2010
+################################################################################
+
+#atk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_ATK) += atk_install
+ROOTFS_COMPONENTS_CLEAN += atk_clean
+ifeq ($(CONFIG_EMBTK_HAVE_ATK),y)
+include $(EMBTK_ROOT)/packages/graphics/atk/atk.mk
+endif
+
+#Cairo
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_CAIRO) += cairo_install
+ROOTFS_COMPONENTS_CLEAN += cairo_clean
+ifeq ($(CONFIG_EMBTK_HAVE_CAIRO),y)
+include $(EMBTK_ROOT)/packages/graphics/cairo/cairo.mk
+endif
+
+#DirectFB
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_DIRECTFB) += directfb_install
+ROOTFS_COMPONENTS_CLEAN += directfb_clean
+ifeq ($(CONFIG_EMBTK_HAVE_DIRECTFB),y)
+include $(EMBTK_ROOT)/packages/graphics/directfb/directfb.mk
+endif
+
+#fontconfig
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FONTCONFIG) += fontconfig_install
+ROOTFS_COMPONENTS_CLEAN += fontconfig_clean
+ifeq ($(CONFIG_EMBTK_HAVE_FONTCONFIG),y)
+include $(EMBTK_ROOT)/packages/graphics/fontconfig/fontconfig.mk
+endif
+
+#FreeFont
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREEFONT_TTF) += freefont_ttf_install
+ROOTFS_COMPONENTS_CLEAN += ttmkfdir_clean
+ifeq ($(CONFIG_EMBTK_HAVE_FREEFONT_TTF),y)
+include $(EMBTK_ROOT)/packages/graphics/freefont/freefont.mk
+endif
+
+#FreeType
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREETYPE) += freetype_install
+ROOTFS_COMPONENTS_CLEAN += freetype_clean
+ifeq ($(CONFIG_EMBTK_HAVE_FREETYPE),y)
+include $(EMBTK_ROOT)/packages/graphics/freetype/freetype.mk
+endif
+
+#gtk+
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GTK) += gtk_install
+ROOTFS_COMPONENTS_CLEAN += gtk_clean
+ifeq ($(CONFIG_EMBTK_HAVE_GTK),y)
+include $(EMBTK_ROOT)/packages/graphics/gtk/gtk.mk
+endif
+
+#libjpeg
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBJPEG) += libjpeg_install
+ROOTFS_COMPONENTS_CLEAN += libjpeg_clean
+ifeq ($(CONFIG_EMBTK_HAVE_LIBJPEG),y)
+include $(EMBTK_ROOT)/packages/graphics/libjpeg/libjpeg.mk
+endif
+
+#libpng
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBPNG) += libpng_clean
+ROOTFS_COMPONENTS_CLEAN += libpng_clean
+ifeq ($(CONFIG_EMBTK_HAVE_LIBPNG),y)
+include $(EMBTK_ROOT)/packages/graphics/libpng/libpng.mk
+endif
+
+#libtiff
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBTIFF) += libtiff_install
+ROOTFS_COMPONENTS_CLEAN += libtiff_clean
+ifeq ($(CONFIG_EMBTK_HAVE_LIBTIFF),y)
+include $(EMBTK_ROOT)/packages/graphics/libtiff/libtiff.mk
+endif
+
+#pixman
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PIXMAN) += pixman_install
+ROOTFS_COMPONENTS_CLEAN += pixman_clean
+ifeq ($(CONFIG_EMBTK_HAVE_PIXMAN),y)
+include $(EMBTK_ROOT)/packages/graphics/pixman/pixman.mk
+endif
+

packages/misc/misc.mk
@@ -1,0 +1,66 @@
+################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+################################################################################
+#
+# \file         misc.mk
+# \brief	misc.mk of Embtoolkit
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         February 2010
+################################################################################
+
+#gettext
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GETTEXT) += gettext_install
+ROOTFS_COMPONENTS_CLEAN += gettext_clean
+ifeq ($(CONFIG_EMBTK_HAVE_GETTEXT),y)
+include $(EMBTK_ROOT)/packages/misc/gettext/gettext.mk
+endif
+
+#GLib
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GLIB) += glib_install
+ROOTFS_COMPONENTS_CLEAN += glib_clean
+ifeq ($(CONFIG_EMBTK_HAVE_GLIB),y)
+include $(EMBTK_ROOT)/packages/misc/glib/glib.mk
+endif
+
+#libelf
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBELF) += libelf_install
+ROOTFS_COMPONENTS_CLEAN += libelf_clean
+ifeq ($(CONFIG_EMBTK_HAVE_LIBELF),y)
+include $(EMBTK_ROOT)/packages/misc/libelf/libelf.mk
+endif
+
+#libxml2
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBXML2) += libxml2_install
+ROOTFS_COMPONENTS_CLEAN += libxml2_clean
+ifeq ($(CONFIG_EMBTK_HAVE_LIBXML2),y)
+include $(EMBTK_ROOT)/packages/misc/libxml/libxml.mk
+endif
+
+#ncurses
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_NCURSES) += ncurses_install
+ROOTFS_COMPONENTS_CLEAN += ncurses_clean
+ifeq ($(CONFIG_EMBTK_HAVE_NCURSES),y)
+include $(EMBTK_ROOT)/packages/misc/ncurses/ncurses.mk
+endif
+
+#Pango
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PANGO) += pango_install
+ROOTFS_COMPONENTS_CLEAN += pango_clean
+ifeq ($(CONFIG_EMBTK_HAVE_PANGO),y)
+include $(EMBTK_ROOT)/packages/misc/pango/pango.mk
+endif
+

packages/scripting-languages/scripting-languages.mk
@@ -1,0 +1,30 @@
+################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+################################################################################
+#
+# \file         scripting-languages.mk
+# \brief	scripting-languages.mk of Embtoolkit
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         February 2010
+################################################################################
+
+#microperl
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_MICROPERL) += microperl_install
+ifeq ($(CONFIG_EMBTK_HAVE_MICROPERL),y)
+include $(EMBTK_ROOT)/packages/scripting-languages/perl/perl.mk
+endif
+

mk/packages.mk
@@ -22,6 +22,8 @@
 # \date         May 2009
 ################################################################################
 
+ROOTFS_COMPONENTS-y :=
+HOSTTOOLS_COMPONENTS-y :=
 ################################################################################
 #################### Common include for target and host ########################
 ################################################################################
@@ -41,194 +43,57 @@ endif
 ################################################################################
 
 #gdb
-ifeq ($(CONFIG_EMBTK_HAVE_GDB_ON_TARGET),y)
-ROOTFS_COMPONENTS += gdb_target_install
-endif
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GDB_ON_TARGET) += gdb_target_install
 
 #gdbserver
-ifeq ($(CONFIG_EMBTK_HAVE_GDBSERVER_ON_TARGET),y)
-ROOTFS_COMPONENTS += gdbserver_target_install
-endif
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GDBSERVER_ON_TARGET) += gdbserver_target_install
 
 #strace
-ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_STRACE),y)
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_ROOTFS_HAVE_STRACE) += strace_install
 include $(EMBTK_ROOT)/mk/strace.mk
-ROOTFS_COMPONENTS += strace_install
-endif
 
-######################## Flash manipulation tools ##############################
-
-#mtd-utils
-ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_MTDUTILS),y)
-ROOTFS_COMPONENTS += mtd-utils_target_install
-endif
+# Flash manipulation tools: mtd-utils
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_ROOTFS_HAVE_MTDUTILS) += mtd-utils_target_install
 ROOTFS_COMPONENTS_CLEAN += mtd-utils_target_clean
 
-######################## Compression packages ##################################
+#Compression packages
 
-########################### Graphics packages ##################################
+# Graphics packages
+include $(EMBTK_ROOT)/packages/graphics/graphics.mk
 
-#atk
-include $(EMBTK_ROOT)/packages/graphics/atk/atk.mk
-ifeq ($(CONFIG_EMBTK_HAVE_ATK),y)
-ROOTFS_COMPONENTS += atk_install
-endif
-ROOTFS_COMPONENTS_CLEAN += atk_clean
+# Networking packages
 
-#Cairo
-include $(EMBTK_ROOT)/packages/graphics/cairo/cairo.mk
-ifeq ($(CONFIG_EMBTK_HAVE_CAIRO),y)
-ROOTFS_COMPONENTS += cairo_install
-endif
-ROOTFS_COMPONENTS_CLEAN += cairo_clean
+# Scripting languages
+include $(EMBTK_ROOT)/packages/scripting-languages/scripting-languages.mk
 
-#DirectFB
-include $(EMBTK_ROOT)/packages/graphics/directfb/directfb.mk
-ifeq ($(CONFIG_EMBTK_HAVE_DIRECTFB),y)
-ROOTFS_COMPONENTS += directfb_install
-endif
-ROOTFS_COMPONENTS_CLEAN += directfb_clean
+# Security packages
+include $(EMBTK_ROOT)/packages/security/security.mk
 
-#fontconfig
-include $(EMBTK_ROOT)/packages/graphics/fontconfig/fontconfig.mk
-ifeq ($(CONFIG_EMBTK_HAVE_FONTCONFIG),y)
-ROOTFS_COMPONENTS += fontconfig_install
-endif
-ROOTFS_COMPONENTS_CLEAN += fontconfig_clean
+# System packages
 
-#FreeFont
-include $(EMBTK_ROOT)/packages/graphics/freefont/freefont.mk
-ifeq ($(CONFIG_EMBTK_HAVE_FREEFONT_TTF),y)
-ROOTFS_COMPONENTS += freefont_ttf_install
-endif
-ROOTFS_COMPONENTS_CLEAN += ttmkfdir_clean
+# Miscellaneous packages
+include $(EMBTK_ROOT)/packages/misc/misc.mk
 
-#FreeType
-include $(EMBTK_ROOT)/packages/graphics/freetype/freetype.mk
-ifeq ($(CONFIG_EMBTK_HAVE_FREETYPE),y)
-ROOTFS_COMPONENTS += freetype_install
-endif
-ROOTFS_COMPONENTS_CLEAN += freetype_clean
-
-#gtk+
-include $(EMBTK_ROOT)/packages/graphics/gtk/gtk.mk
-ifeq ($(CONFIG_EMBTK_HAVE_GTK),y)
-ROOTFS_COMPONENTS += gtk_install
-endif
-ROOTFS_COMPONENTS_CLEAN += gtk_clean
-
-#libjpeg
-include $(EMBTK_ROOT)/packages/graphics/libjpeg/libjpeg.mk
-ifeq ($(CONFIG_EMBTK_HAVE_LIBJPEG),y)
-ROOTFS_COMPONENTS += libjpeg_install
-endif
-ROOTFS_COMPONENTS_CLEAN += libjpeg_clean
-
-#libpng
-include $(EMBTK_ROOT)/packages/graphics/libpng/libpng.mk
-ifeq ($(CONFIG_EMBTK_HAVE_LIBPNG),y)
-ROOTFS_COMPONENTS += libpng_install
-endif
-ROOTFS_COMPONENTS_CLEAN += libpng_clean
-
-#pixman
-include $(EMBTK_ROOT)/packages/graphics/pixman/pixman.mk
-ifeq ($(CONFIG_EMBTK_HAVE_PIXMAN),y)
-ROOTFS_COMPONENTS += pixman_install
-endif
-ROOTFS_COMPONENTS_CLEAN += pixman_clean
-
-#libtiff
-include $(EMBTK_ROOT)/packages/graphics/libtiff/libtiff.mk
-ifeq ($(CONFIG_EMBTK_HAVE_LIBTIFF),y)
-ROOTFS_COMPONENTS += libtiff_install
-endif
-ROOTFS_COMPONENTS_CLEAN += libtiff_clean
-########################## Networking packages #################################
-
-############################ Scripting languages ###############################
-
-ifeq ($(CONFIG_EMBTK_HAVE_MICROPERL),y)
-include $(EMBTK_ROOT)/packages/scripting-languages/perl/perl.mk
-ROOTFS_COMPONENTS += microperl_install
-endif
-############################ Security packages #################################
-
-#OpenSSL
-include $(EMBTK_ROOT)/packages/security/openssl/openssl.mk
-ifeq ($(CONFIG_EMBTK_HAVE_OPENSSL),y)
-ROOTFS_COMPONENTS += openssl_install
-endif
-ROOTFS_COMPONENTS_CLEAN += openssl_clean
-############################ System packages ###################################
-
-########################## Miscellaneous packages ##############################
-
-#gettext
-include $(EMBTK_ROOT)/packages/misc/gettext/gettext.mk
-ifeq ($(CONFIG_EMBTK_HAVE_GETTEXT),y)
-ROOTFS_COMPONENTS += gettext_install
-endif
-ROOTFS_COMPONENTS_CLEAN += gettext_clean
-
-#GLib
-include $(EMBTK_ROOT)/packages/misc/glib/glib.mk
-ifeq ($(CONFIG_EMBTK_HAVE_GLIB),y)
-ROOTFS_COMPONENTS += glib_install
-endif
-ROOTFS_COMPONENTS_CLEAN += glib_clean
-
-#libelf
-include $(EMBTK_ROOT)/packages/misc/libelf/libelf.mk
-ifeq ($(CONFIG_EMBTK_HAVE_LIBELF),y)
-ROOTFS_COMPONENTS += libelf_install
-endif
-ROOTFS_COMPONENTS_CLEAN += libelf_clean
-
-#libxml2
-include $(EMBTK_ROOT)/packages/misc/libxml/libxml.mk
-ifeq ($(CONFIG_EMBTK_HAVE_LIBXML2),y)
-ROOTFS_COMPONENTS += libxml2_install
-endif
-ROOTFS_COMPONENTS_CLEAN += libxml2_clean
-
-#ncurses
-include $(EMBTK_ROOT)/packages/misc/ncurses/ncurses.mk
-ifeq ($(CONFIG_EMBTK_HAVE_NCURSES),y)
-ROOTFS_COMPONENTS += ncurses_install
-endif
-ROOTFS_COMPONENTS_CLEAN += ncurses_clean
-
-#Pango
-include $(EMBTK_ROOT)/packages/misc/pango/pango.mk
-ifeq ($(CONFIG_EMBTK_HAVE_PANGO),y)
-ROOTFS_COMPONENTS += pango_install
-endif
-ROOTFS_COMPONENTS_CLEAN += pango_clean
-################################### BUSYBOX ####################################
 #Busybox
-ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_BB),y)
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_ROOTFS_HAVE_BB) += busybox_install
 include $(EMBTK_ROOT)/packages/busybox/busybox.mk
 ROOTFS_COMPONENTS += busybox_install
-endif
 
 ################################################################################
 ########################## Packages for HOST MACHINE ###########################
 ################################################################################
 
 #gdb
-ifeq ($(CONFIG_EMBTK_HAVE_GDB_ON_HOST),y)
-HOSTTOOLS_COMPONENTS += gdb_host_install
-endif
+HOSTTOOLS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GDB_ON_HOST) += gdb_host_install
 
 ################################################################################
 ########################### Targets for HOST MACHINE ###########################
 ################################################################################
 host_packages_build:
-ifeq ($(HOSTTOOLS_COMPONENTS),)
+ifeq ($(HOSTTOOLS_COMPONENTS-y),)
 else
 	$(call EMBTK_GENERIC_MESSAGE,"Building extra packages intended to run \
 	on your host machine ...")
-	@$(MAKE) $(HOSTTOOLS_COMPONENTS)
+	@$(MAKE) $(HOSTTOOLS_COMPONENTS-y)
 endif
 

mk/rootfs.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
-# Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
+# Copyright(C) 2009-2010 GAYE Abdoulaye Walsimou. All rights reserved.
 #
 # This program is free software; you can distribute it and/or modify it
 # under the terms of the GNU General Public License
@@ -24,6 +24,10 @@
 
 ifeq ($(CONFIG_EMBTK_HAVE_ROOTFS),y)
 
+ROOTFS_HOSTTOOLS-y :=
+ROOTFS_HOSTTOOLS_CLEAN-y :=
+FILESYSTEMS-y :=
+
 #include various filesystems targets
 include $(EMBTK_ROOT)/mk/fs.mk
 
@@ -31,31 +35,28 @@ include $(EMBTK_ROOT)/mk/fs.mk
 include $(EMBTK_ROOT)/mk/fakeroot.mk
 include $(EMBTK_ROOT)/mk/makedevs.mk
 include $(EMBTK_ROOT)/mk/pkgconfig.mk
+ROOTFS_HOSTTOOLS-y += makedevs_install fakeroot_install pkgconfig_install
 ROOTFS_HOSTTOOLS += makedevs_install fakeroot_install pkgconfig_install
 
 #Does CPIO archive for initramfs selected?
-ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_INITRAMFS_CPIO),y)
-FILESYSTEMS += build_initramfs_archive
-endif
+FILESYSTEMS-$(CONFIG_EMBTK_ROOTFS_HAVE_INITRAMFS_CPIO) += build_initramfs_archive
 
 #Does jffs2 filesystem selected?
-ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_JFFS2),y)
-ROOTFS_HOSTTOOLS += mtd-utils_host_install
-ROOTFS_HOSTTOOLS_CLEAN += mtd-utils_host_clean
-FILESYSTEMS += build_jffs2_rootfs
-endif
+ROOTFS_HOSTTOOLS-$(CONFIG_EMBTK_ROOTFS_HAVE_JFFS2) += mtd-utils_host_install
+ROOTFS_HOSTTOOLS_CLEAN-$(CONFIG_EMBTK_ROOTFS_HAVE_JFFS2) += mtd-utils_host_clean
+FILESYSTEMS-$(CONFIG_EMBTK_ROOTFS_HAVE_JFFS2) += build_jffs2_rootfs
 
 #Does squashfs filesystem selected?
+ROOTFS_HOSTTOOLS-$(CONFIG_EMBTK_ROOTFS_HAVE_SQUASHFS) += squashfs_host_install
+FILESYSTEMS-$(CONFIG_EMBTK_ROOTFS_HAVE_SQUASHFS) += build_squashfs_rootfs
 ifeq ($(CONFIG_EMBTK_ROOTFS_HAVE_SQUASHFS),y)
 include $(EMBTK_ROOT)/mk/squashfs.mk
-ROOTFS_HOSTTOOLS += squashfs_host_install
-FILESYSTEMS += build_squashfs_rootfs
 endif
 
 rootfs_build:
 	$(call EMBTK_GENERIC_MESSAGE,"Building selected root filesystems...")
-	@$(MAKE) rootfs_clean mkinitialrootfs $(ROOTFS_HOSTTOOLS) \
-	$(ROOTFS_COMPONENTS) rootfs_fill build_tarbz2_rootfs $(FILESYSTEMS)
+	@$(MAKE) rootfs_clean mkinitialrootfs $(ROOTFS_HOSTTOOLS-y) \
+	$(ROOTFS_COMPONENTS-y) rootfs_fill build_tarbz2_rootfs $(FILESYSTEMS-y)
 	$(Q)rm -rf $(ROOTFS)
 	$(call EMBTK_GENERIC_MESSAGE,"Build of selected root filesystems \
 	ended successfully!")

packages/security/security.mk
@@ -1,0 +1,31 @@
+################################################################################
+# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
+#
+# This program is free software; you can distribute it and/or modify it
+# under the terms of the GNU General Public License
+# (Version 2 or later) published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+################################################################################
+#
+# \file         security.mk
+# \brief	security.mk of Embtoolkit
+# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \date         February 2010
+################################################################################
+
+#OpenSSL
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_OPENSSL) += openssl_install
+ROOTFS_COMPONENTS_CLEAN += openssl_clean
+ifeq ($(CONFIG_EMBTK_HAVE_OPENSSL),y)
+include $(EMBTK_ROOT)/packages/security/openssl/openssl.mk
+endif
+

commit 1d48633090fba808b4e61af5f7d15931969bf1b9

Packages: fix build error introduced by 95b7b617b53eb2
Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

packages/graphics/graphics.mk
@@ -23,52 +23,52 @@
 ################################################################################
 
 #atk
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_ATK) += atk_install
+include $(EMBTK_ROOT)/packages/graphics/atk/atk.mk
 ROOTFS_COMPONENTS_CLEAN += atk_clean
 ifeq ($(CONFIG_EMBTK_HAVE_ATK),y)
-include $(EMBTK_ROOT)/packages/graphics/atk/atk.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_ATK) += atk_install
 endif
 
 #Cairo
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_CAIRO) += cairo_install
+include $(EMBTK_ROOT)/packages/graphics/cairo/cairo.mk
 ROOTFS_COMPONENTS_CLEAN += cairo_clean
 ifeq ($(CONFIG_EMBTK_HAVE_CAIRO),y)
-include $(EMBTK_ROOT)/packages/graphics/cairo/cairo.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_CAIRO) += cairo_install
 endif
 
 #DirectFB
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_DIRECTFB) += directfb_install
+include $(EMBTK_ROOT)/packages/graphics/directfb/directfb.mk
 ROOTFS_COMPONENTS_CLEAN += directfb_clean
 ifeq ($(CONFIG_EMBTK_HAVE_DIRECTFB),y)
-include $(EMBTK_ROOT)/packages/graphics/directfb/directfb.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_DIRECTFB) += directfb_install
 endif
 
 #fontconfig
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FONTCONFIG) += fontconfig_install
+include $(EMBTK_ROOT)/packages/graphics/fontconfig/fontconfig.mk
 ROOTFS_COMPONENTS_CLEAN += fontconfig_clean
 ifeq ($(CONFIG_EMBTK_HAVE_FONTCONFIG),y)
-include $(EMBTK_ROOT)/packages/graphics/fontconfig/fontconfig.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FONTCONFIG) += fontconfig_install
 endif
 
 #FreeFont
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREEFONT_TTF) += freefont_ttf_install
+include $(EMBTK_ROOT)/packages/graphics/freefont/freefont.mk
 ROOTFS_COMPONENTS_CLEAN += ttmkfdir_clean
 ifeq ($(CONFIG_EMBTK_HAVE_FREEFONT_TTF),y)
-include $(EMBTK_ROOT)/packages/graphics/freefont/freefont.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREEFONT_TTF) += freefont_ttf_install
 endif
 
 #FreeType
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREETYPE) += freetype_install
+include $(EMBTK_ROOT)/packages/graphics/freetype/freetype.mk
 ROOTFS_COMPONENTS_CLEAN += freetype_clean
 ifeq ($(CONFIG_EMBTK_HAVE_FREETYPE),y)
-include $(EMBTK_ROOT)/packages/graphics/freetype/freetype.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_FREETYPE) += freetype_install
 endif
 
 #gtk+
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GTK) += gtk_install
+include $(EMBTK_ROOT)/packages/graphics/gtk/gtk.mk
 ROOTFS_COMPONENTS_CLEAN += gtk_clean
 ifeq ($(CONFIG_EMBTK_HAVE_GTK),y)
-include $(EMBTK_ROOT)/packages/graphics/gtk/gtk.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GTK) += gtk_install
 endif
 
 #libjpeg
@@ -79,23 +79,23 @@ include $(EMBTK_ROOT)/packages/graphics/libjpeg/libjpeg.mk
 endif
 
 #libpng
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBPNG) += libpng_clean
+include $(EMBTK_ROOT)/packages/graphics/libpng/libpng.mk
 ROOTFS_COMPONENTS_CLEAN += libpng_clean
 ifeq ($(CONFIG_EMBTK_HAVE_LIBPNG),y)
-include $(EMBTK_ROOT)/packages/graphics/libpng/libpng.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBPNG) += libpng_clean
 endif
 
 #libtiff
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBTIFF) += libtiff_install
+include $(EMBTK_ROOT)/packages/graphics/libtiff/libtiff.mk
 ROOTFS_COMPONENTS_CLEAN += libtiff_clean
 ifeq ($(CONFIG_EMBTK_HAVE_LIBTIFF),y)
-include $(EMBTK_ROOT)/packages/graphics/libtiff/libtiff.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBTIFF) += libtiff_install
 endif
 
 #pixman
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PIXMAN) += pixman_install
+include $(EMBTK_ROOT)/packages/graphics/pixman/pixman.mk
 ROOTFS_COMPONENTS_CLEAN += pixman_clean
 ifeq ($(CONFIG_EMBTK_HAVE_PIXMAN),y)
-include $(EMBTK_ROOT)/packages/graphics/pixman/pixman.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PIXMAN) += pixman_install
 endif
 

packages/misc/misc.mk
@@ -23,44 +23,44 @@
 ################################################################################
 
 #gettext
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GETTEXT) += gettext_install
+include $(EMBTK_ROOT)/packages/misc/gettext/gettext.mk
 ROOTFS_COMPONENTS_CLEAN += gettext_clean
 ifeq ($(CONFIG_EMBTK_HAVE_GETTEXT),y)
-include $(EMBTK_ROOT)/packages/misc/gettext/gettext.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GETTEXT) += gettext_install
 endif
 
 #GLib
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GLIB) += glib_install
+include $(EMBTK_ROOT)/packages/misc/glib/glib.mk
 ROOTFS_COMPONENTS_CLEAN += glib_clean
 ifeq ($(CONFIG_EMBTK_HAVE_GLIB),y)
-include $(EMBTK_ROOT)/packages/misc/glib/glib.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_GLIB) += glib_install
 endif
 
 #libelf
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBELF) += libelf_install
+include $(EMBTK_ROOT)/packages/misc/libelf/libelf.mk
 ROOTFS_COMPONENTS_CLEAN += libelf_clean
 ifeq ($(CONFIG_EMBTK_HAVE_LIBELF),y)
-include $(EMBTK_ROOT)/packages/misc/libelf/libelf.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBELF) += libelf_install
 endif
 
 #libxml2
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBXML2) += libxml2_install
+include $(EMBTK_ROOT)/packages/misc/libxml/libxml.mk
 ROOTFS_COMPONENTS_CLEAN += libxml2_clean
 ifeq ($(CONFIG_EMBTK_HAVE_LIBXML2),y)
-include $(EMBTK_ROOT)/packages/misc/libxml/libxml.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBXML2) += libxml2_install
 endif
 
 #ncurses
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_NCURSES) += ncurses_install
+include $(EMBTK_ROOT)/packages/misc/ncurses/ncurses.mk
 ROOTFS_COMPONENTS_CLEAN += ncurses_clean
 ifeq ($(CONFIG_EMBTK_HAVE_NCURSES),y)
-include $(EMBTK_ROOT)/packages/misc/ncurses/ncurses.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_NCURSES) += ncurses_install
 endif
 
 #Pango
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PANGO) += pango_install
+include $(EMBTK_ROOT)/packages/misc/pango/pango.mk
 ROOTFS_COMPONENTS_CLEAN += pango_clean
 ifeq ($(CONFIG_EMBTK_HAVE_PANGO),y)
-include $(EMBTK_ROOT)/packages/misc/pango/pango.mk
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_PANGO) += pango_install
 endif
 

packages/scripting-languages/scripting-languages.mk
@@ -23,8 +23,8 @@
 ################################################################################
 
 #microperl
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_MICROPERL) += microperl_install
-ifeq ($(CONFIG_EMBTK_HAVE_MICROPERL),y)
 include $(EMBTK_ROOT)/packages/scripting-languages/perl/perl.mk
+ifeq ($(CONFIG_EMBTK_HAVE_MICROPERL),y)
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_MICROPERL) += microperl_install
 endif
 