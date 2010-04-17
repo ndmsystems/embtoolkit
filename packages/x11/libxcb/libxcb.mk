23888b27cfcf7c2e64b70df668679459f6031632
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2010-04-17 21:26:33 +0200

    Packages: X11/libxcb: enable xinput
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- packages/x11/libxcb/libxcb.mk
+++ packages/x11/libxcb/libxcb.mk
@@ -80,7 +80,7 @@ $(LIBXCB_BUILD_DIR)/.configured:
 	PKG_CONFIG=$(PKGCONFIG_BIN) \
 	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
 	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) \
+	--target=$(STRICT_GNU_TARGET) --enable-xinput \
 	--prefix=/usr --libdir=/usr/$(LIBDIR)
 	@touch $@
 
