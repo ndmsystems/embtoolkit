f8299fa80b1a1fd5572da35a593d26b0f7c6298a
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2010-04-18 20:00:16 +0200

    Rootfs: separate device nodes build and tar.bz2 rootfs build
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- mk/fs.mk
+++ mk/fs.mk
@@ -1,19 +1,20 @@
 ################################################################################
 # GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
-# Copyright(C) 2009 GAYE Abdoulaye Walsimou. All rights reserved.
+# Copyright(C) 2009-2010 GAYE Abdoulaye Walsimou. All rights reserved.
 #
-# This program is free software; you can distribute it and/or modify it
-# under the terms of the GNU General Public License
-# (Version 2 or later) published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ################################################################################
 #
 # \file         fs.mk
@@ -22,12 +23,16 @@
 # \date         August 2009
 ################################################################################
 
-build_tarbz2_rootfs:
-	$(call EMBTK_GENERIC_MESSAGE,"Generating tar.bz2 file of the root \
+build_rootfs_devnodes:
+	$(call EMBTK_GENERIC_MESSAGE,"Populating device nodes of the root \
 	filesystem...")
 	@$(FAKEROOT_BIN) -s $(EMBTK_ROOT)/.fakeroot.001 -- \
 	$(MAKEDEVS_DIR)/makedevs \
 	-d $(EMBTK_ROOT)/src/devices_table.txt $(ROOTFS)
+
+build_tarbz2_rootfs:
+	$(call EMBTK_GENERIC_MESSAGE,"Generating tar.bz2 file of the root \
+	filesystem...")
 	@cd $(ROOTFS) ; $(FAKEROOT_BIN) -i $(EMBTK_ROOT)/.fakeroot.001 -- \
 	tar cjf rootfs-$(GNU_TARGET)-$(EMBTK_MCU_FLAG).tar.bz2 * ; \
 	mv rootfs-$(GNU_TARGET)-$(EMBTK_MCU_FLAG).tar.bz2 $(EMBTK_ROOT)
