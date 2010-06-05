8d8ef7dd093b2fef0090db79c740e454530de04c
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2010-06-05 23:24:36 +0200

    packages: X11/libxext: fix malloc(0) behaviour
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- packages/x11/libxext/libxext.mk
+++ packages/x11/libxext/libxext.mk
@@ -88,7 +88,7 @@ $(LIBXEXT_BUILD_DIR)/.configured:
 	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
 	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
 	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr --enable-malloc0returnsnull
+	--prefix=/usr --disable-malloc0returnsnull
 	@touch $@
 
 libxext_clean:
