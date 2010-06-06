61d274c7f1b8172706e652907f89b163c1e2fe5d
Abdoulaye Walsimou Gaye <walsimou@walsimou.com>
2010-06-06 14:53:01 +0200

    Packages: X11: update license and set --disable-malloc0returnsnull for various packages
    Signed-off-by: Abdoulaye Walsimou Gaye <walsimou@walsimou.com>

--- packages/x11/randrproto/randrproto.mk
+++ packages/x11/randrproto/randrproto.mk
@@ -2,18 +2,19 @@
 # GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
 # Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
 #
-# This program is free software; you can distribute it and/or modify it
-# under the terms of the GNU General Public License
-# (Version 2 or later) published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ################################################################################
 #
 # \file         randrproto.mk
@@ -82,8 +83,8 @@ $(RANDRPROTO_BUILD_DIR)/.configured:
 	PKG_CONFIG=$(PKGCONFIG_BIN) \
 	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
 	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) \
-	--prefix=/usr --libdir=/usr/$(LIBDIR)
+	--target=$(STRICT_GNU_TARGET) --prefix=/usr --libdir=/usr/$(LIBDIR) \
+	--disable-malloc0returnsnull
 	@touch $@
 
 randrproto_clean:
