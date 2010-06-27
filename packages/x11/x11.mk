6177455ee85e04ed5b0946a598ecb10552c6e42a
Abdoulaye Walsimou Gaye <awg@embtoolkit.com>
2010-06-27 10:22:18 +0200

    Packages: X11: remove likely obsolete xinerama (continued)
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.com>

--- packages/x11/x11.mk
+++ packages/x11/x11.mk
@@ -113,11 +113,6 @@ include $(EMBTK_ROOT)/packages/x11/xextproto/xextproto.mk
 ROOTFS_COMPONENTS_CLEAN += xextproto_clean
 ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_XEXTPROTO) += xextproto_install
 
-#xineramaproto
-include $(EMBTK_ROOT)/packages/x11/xineramaproto/xineramaproto.mk
-ROOTFS_COMPONENTS_CLEAN += xineramaproto_clean
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_XINERAMAPROTO) += xineramaproto_install
-
 #xproto
 include $(EMBTK_ROOT)/packages/x11/xproto/xproto.mk
 ROOTFS_COMPONENTS_CLEAN += xproto_clean
@@ -145,11 +140,6 @@ ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBPTHREADSTUBS) += libpthreadstubs_instal
 include $(EMBTK_ROOT)/packages/x11/libxau/libxau.mk
 ROOTFS_COMPONENTS_CLEAN += libxau_clean
 ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBXAU) += libxau_install
-
-#libxinerama
-include $(EMBTK_ROOT)/packages/x11/libxinerama/libxinerama.mk
-ROOTFS_COMPONENTS_CLEAN += libxinerama_clean
-ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LIBXINERAMA) += libxinerama_install
 
 #libxcb
 include $(EMBTK_ROOT)/packages/x11/libxcb/libxcb.mk
