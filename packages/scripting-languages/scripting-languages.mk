80eb1eea52b0edbe77fcef35fc980be9c345ceb5
Abdoulaye Walsimou Gaye <awg@embtoolkit.com>
2010-07-03 11:50:54 +0200

    Packages: New Lua: add lua-5.1 among scripting languages
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.com>

--- packages/scripting-languages/scripting-languages.mk
+++ packages/scripting-languages/scripting-languages.mk
@@ -1,26 +1,32 @@
 ################################################################################
-# GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
-# Copyright(C) 2010 GAYE Abdoulaye Walsimou. All rights reserved.
+# Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
+# Copyright(C) 2010 Abdoulaye Walsimou GAYE. All rights reserved.
 #
-# This program is free software; you can distribute it and/or modify it
-# under the terms of the GNU General Public License
-# (Version 2 or later) published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ################################################################################
 #
 # \file         scripting-languages.mk
 # \brief	scripting-languages.mk of Embtoolkit
-# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
+# \author       Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
 # \date         February 2010
 ################################################################################
+
+#lua
+include $(EMBTK_ROOT)/packages/scripting-languages/lua/lua.mk
+ROOTFS_COMPONENTS_CLEAN += lua_clean
+ROOTFS_COMPONENTS-$(CONFIG_EMBTK_HAVE_LUA) += lua_install
 
 #microperl
 include $(EMBTK_ROOT)/packages/scripting-languages/perl/perl.mk
