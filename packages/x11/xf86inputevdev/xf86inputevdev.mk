4f0633212bfc69deb632048ae5921da8d606d6a2
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-01-30 18:45:23 +0100

    Packages:X11/xf86-input-evdev: add xf86-input-evdev-2.6.0 and convert it to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/xf86inputevdev/xf86inputevdev.mk
+++ packages/x11/xf86inputevdev/xf86inputevdev.mk
@@ -23,16 +23,19 @@
 # \date         July 2010
 ################################################################################
 
+XF86INPUTEVDEV_NAME := xf86-input-evdev
 XF86INPUTEVDEV_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_XF86INPUTEVDEV_VERSION_STRING)))
 XF86INPUTEVDEV_SITE := http://xorg.freedesktop.org/archive/individual/driver
+XF86INPUTEVDEV_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 XF86INPUTEVDEV_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/xf86-input-evdev/$(XF86INPUTEVDEV_VERSION)
 XF86INPUTEVDEV_PACKAGE := xf86-input-evdev-$(XF86INPUTEVDEV_VERSION).tar.bz2
+XF86INPUTEVDEV_SRC_DIR := $(PACKAGES_BUILD)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION)
 XF86INPUTEVDEV_BUILD_DIR := $(PACKAGES_BUILD)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION)
 
 XF86INPUTEVDEV_BINS =
 XF86INPUTEVDEV_SBINS =
-XF86INPUTEVDEV_INCLUDES = xorg
-XF86INPUTEVDEV_LIBS = xorg
+XF86INPUTEVDEV_INCLUDES = xorg/evdev-properties.h
+XF86INPUTEVDEV_LIBS = xorg/modules/input/evdev_drv.*
 XF86INPUTEVDEV_PKGCONFIGS = xorg-evdev.pc
 
 XF86INPUTEVDEV_DEPS = xserver_install
@@ -47,7 +50,6 @@ $(XF86INPUTEVDEV_BUILD_DIR)/.installed: $(XF86INPUTEVDEV_DEPS) \
 	$(XF86INPUTEVDEV_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	xf86inputevdev-$(XF86INPUTEVDEV_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(XF86INPUTEVDEV_BUILD_DIR))
 	$(Q)$(MAKE) -C $(XF86INPUTEVDEV_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(XF86INPUTEVDEV_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -55,57 +57,16 @@ $(XF86INPUTEVDEV_BUILD_DIR)/.installed: $(XF86INPUTEVDEV_DEPS) \
 	@touch $@
 
 download_xf86inputevdev:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(XF86INPUTEVDEV_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(XF86INPUTEVDEV_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(XF86INPUTEVDEV_PACKAGE) \
-	$(XF86INPUTEVDEV_SITE)/$(XF86INPUTEVDEV_PACKAGE)
-ifeq ($(CONFIG_EMBTK_XF86INPUTEVDEV_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION).patch \
-	$(XF86INPUTEVDEV_PATCH_SITE)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,XF86INPUTEVDEV)
 
 $(XF86INPUTEVDEV_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(XF86INPUTEVDEV_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjf $(DOWNLOAD_DIR)/$(XF86INPUTEVDEV_PACKAGE)
-ifeq ($(CONFIG_EMBTK_XF86INPUTEVDEV_NEED_PATCH),y)
-	@cd $(PACKAGES_BUILD)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION); \
-	patch -p1 < $(DOWNLOAD_DIR)/xf86-input-evdev-$(XF86INPUTEVDEV_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,XF86INPUTEVDEV)
 
 $(XF86INPUTEVDEV_BUILD_DIR)/.configured:
-	$(Q)cd $(XF86INPUTEVDEV_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,XF86INPUTEVDEV)
 
 xf86inputevdev_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup xf86inputevdev...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(XF86INPUTEVDEV_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(XF86INPUTEVDEV_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(XF86INPUTEVDEV_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(XF86INPUTEVDEV_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(XF86INPUTEVDEV_PKGCONFIGS)
-	$(Q)-rm -rf $(XF86INPUTEVDEV_BUILD_DIR)*
+	$(call EMBTK_CLEANUP,XF86INPUTEVDEV)
 
 .PHONY: $(XF86INPUTEVDEV_BUILD_DIR)/.special
 
