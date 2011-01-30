50af16dfca66ec250c584dc946f7c4d213421cf2
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-01-30 12:13:29 +0100

    Packages: libevent: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/development/libevent/libevent.mk
+++ packages/development/libevent/libevent.mk
@@ -23,10 +23,13 @@
 # \date         December 2009
 ################################################################################
 
-LIBEVENT_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBEVENT_VERSION_STRING)))
+LIBEVENT_NAME := libevent
+LIBEVENT_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBEVENT)
 LIBEVENT_SITE := http://www.monkey.org/~provos
+LIBEVENT_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBEVENT_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libevent/$(LIBEVENT_VERSION)
 LIBEVENT_PACKAGE := libevent-$(LIBEVENT_VERSION).tar.gz
+LIBEVENT_SRC_DIR := $(PACKAGES_BUILD)/libevent-$(LIBEVENT_VERSION)
 LIBEVENT_BUILD_DIR := $(PACKAGES_BUILD)/libevent-$(LIBEVENT_VERSION)
 
 LIBEVENT_BINS = event_rpcgen.py
@@ -45,7 +48,6 @@ $(LIBEVENT_BUILD_DIR)/.installed: $(LIBEVENT_DEPS) download_libevent \
 	$(LIBEVENT_BUILD_DIR)/.decompressed $(LIBEVENT_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libevent-$(LIBEVENT_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(LIBEVENT_BUILD_DIR))
 	$(Q)$(MAKE) -C $(LIBEVENT_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(LIBEVENT_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -53,55 +55,13 @@ $(LIBEVENT_BUILD_DIR)/.installed: $(LIBEVENT_DEPS) download_libevent \
 	@touch $@
 
 download_libevent:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBEVENT_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBEVENT_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBEVENT_PACKAGE) \
-	$(LIBEVENT_SITE)/$(LIBEVENT_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBEVENT_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/libevent-$(LIBEVENT_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/libevent-$(LIBEVENT_VERSION).patch \
-	$(LIBEVENT_PATCH_SITE)/libevent-$(LIBEVENT_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,LIBEVENT)
 
 $(LIBEVENT_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBEVENT_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xzf $(DOWNLOAD_DIR)/$(LIBEVENT_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBEVENT_NEED_PATCH),y)
-	@cd $(LIBEVENT_BUILD_DIR); \
-	patch -p1 < $(DOWNLOAD_DIR)/libevent-$(LIBEVENT_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBEVENT)
 
 $(LIBEVENT_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBEVENT_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLAGS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBEVENT)
 
 libevent_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libevent...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBEVENT_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBEVENT_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBEVENT_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBEVENT_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBEVENT_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBEVENT_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBEVENT)
