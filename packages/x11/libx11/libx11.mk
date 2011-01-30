cb7b6ccb98839589d834e263ffe68b9653ae291a
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-01-30 17:29:38 +0100

    Packages: X11/libx11: add libX11 and convert it to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libx11/libx11.mk
+++ packages/x11/libx11/libx11.mk
@@ -23,9 +23,12 @@
 # \date         March 2010
 ################################################################################
 
-LIBX11_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBX11_VERSION_STRING)))
+LIBX11_NAME := libX11
+LIBX11_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBX11)
 LIBX11_SITE := http://xorg.freedesktop.org/archive/individual/lib
+LIBX11_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBX11_PACKAGE := libX11-$(LIBX11_VERSION).tar.bz2
+LIBX11_SRC_DIR := $(PACKAGES_BUILD)/libX11-$(LIBX11_VERSION)
 LIBX11_BUILD_DIR := $(PACKAGES_BUILD)/libX11-$(LIBX11_VERSION)
 
 LIBX11_BINS =
@@ -35,6 +38,9 @@ LIBX11_INCLUDES = X11/cursorfont.h X11/ImUtil.h X11/Xcms.h X11/XKBlib.h \
 		X11/Xlocale.h X11/Xregion.h X11/Xresource.h X11/Xutil.h
 LIBX11_LIBS = libX11* X11/Xcms.txt
 LIBX11_PKGCONFIGS = x11.pc x11-xcb.pc
+
+LIBX11_CONFIGURE_OPTS := --with-xcb --without-xmlto --without-ps2pdf \
+	--without-groff --disable-malloc0returnsnull
 
 LIBX11_DEPS = utilmacros_install inputproto_install kbproto_install \
 		xextproto_install xproto_install libxcb_install xtrans_install
@@ -48,7 +54,6 @@ $(LIBX11_BUILD_DIR)/.installed: $(LIBX11_DEPS) download_libx11 \
 	$(LIBX11_BUILD_DIR)/.decompressed $(LIBX11_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libx11-$(LIBX11_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(LIBX11_BUILD_DIR))
 	@cd $(LIBX11_BUILD_DIR)/src/util; \
 	gcc makekeys.c -c -o makekeys-makekeys.o; gcc makekeys.c -o makekeys
 	$(Q)$(MAKE) -C $(LIBX11_BUILD_DIR) $(J)
@@ -59,48 +64,16 @@ $(LIBX11_BUILD_DIR)/.installed: $(LIBX11_DEPS) download_libx11 \
 	@touch $@
 
 download_libx11:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBX11_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBX11_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBX11_PACKAGE) \
-	$(LIBX11_SITE)/$(LIBX11_PACKAGE)
+	$(call EMBTK_DOWNLOAD_PKG,LIBX11)
 
 $(LIBX11_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBX11_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjvf $(DOWNLOAD_DIR)/$(LIBX11_PACKAGE)
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBX11)
 
 $(LIBX11_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBX11_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --prefix=/usr --libdir=/usr/$(LIBDIR) \
-	--with-xcb --disable-malloc0returnsnull
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBX11)
 
 libx11_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libx11-$(LIBX11_VERSION)...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBX11_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBX11_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBX11_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBX11_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBX11_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBX11_BUILD_DIR)*
+	$(call EMBTK_CLEANUP_PKG,LIBX11)
 
 $(LIBX11_BUILD_DIR)/.patchlibtool:
 	@LIBX11_LT_FILES=`find $(SYSROOT)/usr/$(LIBDIR)/libX11-* -type f -name *.la`; \
