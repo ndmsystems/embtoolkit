80d6233691bddaecbe9e8b40ecbb4f3559d158c0
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-13 14:18:14 +0100

    Packages: libpng: convert to use generic macro
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/graphics/libpng/libpng.mk
+++ packages/graphics/libpng/libpng.mk
@@ -23,9 +23,13 @@
 # \date         October 2009
 ################################################################################
 
-LIBPNG_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBPNG_VERSION_STRING)))
+LIBPNG_NAME := libpng
+LIBPNG_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBPNG)
 LIBPNG_SITE := http://download.sourceforge.net/libpng
 LIBPNG_PACKAGE := libpng-$(LIBPNG_VERSION).tar.gz
+LIBPNG_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+LIBPNG_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libpng/$(LIBPNG_VERSION)
+LIBPNG_SRC_DIR := $(PACKAGES_BUILD)/libpng-$(LIBPNG_VERSION)
 LIBPNG_BUILD_DIR := $(PACKAGES_BUILD)/libpng-$(LIBPNG_VERSION)
 
 LIBPNG_BINS = libpng*
@@ -33,6 +37,8 @@ LIBPNG_SBINS =
 LIBPNG_INCLUDES = libpng* png*
 LIBPNG_LIBS = libpng*
 LIBPNG_PKGCONFIGS = libpng*
+
+LIBPNG_CONFIGURE_OPTS := --with-libpng-compat=no
 
 LIBPNG_DEPS := zlib_target_install
 
@@ -44,7 +50,6 @@ $(LIBPNG_BUILD_DIR)/.installed:  $(LIBPNG_DEPS) download_libpng \
 	$(LIBPNG_BUILD_DIR)/.decompressed $(LIBPNG_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libpng-$(LIBPNG_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH, $(LIBPNG_BUILD_DIR))
 	$(Q)$(MAKE) -C $(LIBPNG_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(LIBPNG_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -52,46 +57,13 @@ $(LIBPNG_BUILD_DIR)/.installed:  $(LIBPNG_DEPS) download_libpng \
 	@touch $@
 
 download_libpng:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBPNG_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBPNG_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBPNG_PACKAGE) \
-	$(LIBPNG_SITE)/$(LIBPNG_PACKAGE)
+	$(call EMBTK_DOWNLOAD_PKG,LIBPNG)
 
 $(LIBPNG_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBPNG_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xzvf $(DOWNLOAD_DIR)/$(LIBPNG_PACKAGE)
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBPNG)
 
 $(LIBPNG_BUILD_DIR)/.configured:
-	cd $(LIBPNG_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLAGS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--prefix=/usr --enable-static=no --with-libpng-compat=no \
-	--libdir=/usr/$(LIBDIR)
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBPNG)
 
 libpng_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libpng-$(LIBPNG_VERSION)...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBPNG_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBPNG_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBPNG_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBPNG_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBPNG_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBPNG_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBPNG)
