3527b12618568db8ed32ea8c9dbb9f8356f4e3d2
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-13 22:12:26 +0100

    Packages: expat: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/misc/expat/expat.mk
+++ packages/misc/expat/expat.mk
@@ -23,10 +23,13 @@
 # \date         July 2010
 ################################################################################
 
-EXPAT_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_EXPAT_VERSION_STRING)))
+EXPAT_NAME := expat
+EXPAT_VERSION := $(call EMBTK_GET_PKG_VERSION,EXPAT)
 EXPAT_SITE := http://downloads.sourceforge.net/expat
+EXPAT_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 EXPAT_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/expat/$(EXPAT_VERSION)
 EXPAT_PACKAGE := expat-$(EXPAT_VERSION).tar.gz
+EXPAT_SRC_DIR := $(PACKAGES_BUILD)/expat-$(EXPAT_VERSION)
 EXPAT_BUILD_DIR := $(PACKAGES_BUILD)/expat-$(EXPAT_VERSION)
 
 EXPAT_BINS = xmlwf
@@ -52,55 +55,13 @@ $(EXPAT_BUILD_DIR)/.installed: $(EXPAT_DEPS) download_expat \
 	@touch $@
 
 download_expat:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(EXPAT_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(EXPAT_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(EXPAT_PACKAGE) \
-	$(EXPAT_SITE)/$(EXPAT_PACKAGE)
-ifeq ($(CONFIG_EMBTK_EXPAT_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/expat-$(EXPAT_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/expat-$(EXPAT_VERSION).patch \
-	$(EXPAT_PATCH_SITE)/expat-$(EXPAT_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,EXPAT)
 
 $(EXPAT_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(EXPAT_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xzf $(DOWNLOAD_DIR)/$(EXPAT_PACKAGE)
-ifeq ($(CONFIG_EMBTK_EXPAT_NEED_PATCH),y)
-	@cd $(PACKAGES_BUILD)/expat-$(EXPAT_VERSION); \
-	patch -p1 < $(DOWNLOAD_DIR)/expat-$(EXPAT_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,EXPAT)
 
 $(EXPAT_BUILD_DIR)/.configured:
-	$(Q)cd $(EXPAT_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLAGS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,EXPAT)
 
 expat_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup expat...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(EXPAT_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(EXPAT_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(EXPAT_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(EXPAT_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(EXPAT_PKGCONFIGS)
-	$(Q)-rm -rf $(EXPAT_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,EXPAT)
