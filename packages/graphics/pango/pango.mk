1af880d76f79bc5243056797c921c65a6d7131a9
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-13 19:28:45 +0100

    Packages: pango: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/graphics/pango/pango.mk
+++ packages/graphics/pango/pango.mk
@@ -23,10 +23,14 @@
 # \date         December 2009
 ################################################################################
 
-PANGO_MAJOR_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_PANGO_MAJOR_VERSION_STRING)))
-PANGO_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_PANGO_VERSION_STRING)))
+PANGO_NAME := pango
+PANGO_MAJOR_VERSION := $(call EMBTK_GET_PKG_VERSION,PANGO_MAJOR)
+PANGO_VERSION := $(call EMBTK_GET_PKG_VERSION,PANGO)
 PANGO_SITE := http://ftp.gnome.org/pub/gnome/sources/pango/$(PANGO_MAJOR_VERSION)
+PANGO_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+PANGO_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/pango/$(PANGO_VERSION)
 PANGO_PACKAGE := pango-$(PANGO_VERSION).tar.bz2
+PANGO_SRC_DIR := $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
 PANGO_BUILD_DIR := $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
 
 PANGO_BINS = pango*
@@ -53,7 +57,6 @@ $(PANGO_BUILD_DIR)/.installed: $(PANGO_DEPS) download_pango \
 	$(PANGO_BUILD_DIR)/.decompressed $(PANGO_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	pango-$(PANGO_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH, $(PANGO_BUILD_DIR))
 	$(Q)$(MAKE) -C $(PANGO_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(PANGO_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -62,40 +65,13 @@ $(PANGO_BUILD_DIR)/.installed: $(PANGO_DEPS) download_pango \
 	@touch $@
 
 download_pango:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(PANGO_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(PANGO_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(PANGO_PACKAGE) \
-	$(PANGO_SITE)/$(PANGO_PACKAGE)
+	$(call EMBTK_DOWNLOAD_PKG,PANGO)
 
 $(PANGO_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(PANGO_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjvf $(DOWNLOAD_DIR)/$(PANGO_PACKAGE)
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,PANGO)
 
 $(PANGO_BUILD_DIR)/.configured:
-	$(Q)cd $(PANGO_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLAGS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	PKG_CONFIG_LIBDIR=$(SYSROOT)/usr/lib \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr $(PANGO_CONFIGURE_OPTS)
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,PANGO)
 
 $(PANGO_BUILD_DIR)/.patchlibtool:
 	$(Q)PANGO_LT_FILES=`find $(SYSROOT)/usr/$(LIBDIR)/* -type f -name *.la`; \
@@ -112,17 +88,7 @@ $(PANGO_BUILD_DIR)/.patchlibtool:
 
 $(PANGO_BUILD_DIR)/.special:
 	$(Q)-cp -R $(SYSROOT)/usr/$(LIBDIR)/pango $(ROOTFS)/usr/$(LIBDIR)/
-	$(Q)-mkdir -p $(ROOTFS)/usr
-	$(Q)-mkdir -p $(ROOTFS)/usr/etc
-	$(Q)-cp -R $(SYSROOT)/usr/etc/pango $(ROOTFS)/usr/etc/
 	@touch $@
 
 pango_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup pango-$(PANGO_VERSION)...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(PANGO_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(PANGO_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(PANGO_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(PANGO_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(PANGO_PKGCONFIGS)
-	$(Q)-rm -rf $(PANGO_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,PANGO)
