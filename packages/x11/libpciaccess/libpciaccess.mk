7175c9089890f5774a5c479d19848bca9dc84de8
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-20 23:02:32 +0100

    Packages: X11/libpciaccess: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libpciaccess/libpciaccess.mk
+++ packages/x11/libpciaccess/libpciaccess.mk
@@ -23,10 +23,13 @@
 # \date         March 2010
 ################################################################################
 
-LIBPCIACCESS_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBPCIACCESS_VERSION_STRING)))
+LIBPCIACCESS_NAME := libpciaccess
+LIBPCIACCESS_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBPCIACCESS)
 LIBPCIACCESS_SITE := http://xorg.freedesktop.org/archive/individual/lib
 LIBPCIACCESS_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libpciaccess/$(LIBPCIACCESS_VERSION)
+LIBPCIACCESS_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBPCIACCESS_PACKAGE := libpciaccess-$(LIBPCIACCESS_VERSION).tar.gz
+LIBPCIACCESS_SRC_DIR := $(PACKAGES_BUILD)/libpciaccess-$(LIBPCIACCESS_VERSION)
 LIBPCIACCESS_BUILD_DIR := $(PACKAGES_BUILD)/libpciaccess-$(LIBPCIACCESS_VERSION)
 
 LIBPCIACCESS_BINS =
@@ -46,7 +49,6 @@ $(LIBPCIACCESS_BUILD_DIR)/.installed: $(LIBPCIACCESS_DEPS) \
 	$(LIBPCIACCESS_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libpciaccess-$(LIBPCIACCESS_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(LIBPCIACCESS_BUILD_DIR))
 	$(Q)$(MAKE) -C $(LIBPCIACCESS_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(LIBPCIACCESS_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -54,55 +56,13 @@ $(LIBPCIACCESS_BUILD_DIR)/.installed: $(LIBPCIACCESS_DEPS) \
 	@touch $@
 
 download_libpciaccess:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBPCIACCESS_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBPCIACCESS_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBPCIACCESS_PACKAGE) \
-	$(LIBPCIACCESS_SITE)/$(LIBPCIACCESS_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBPCIACCESS_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/libpciaccess-$(LIBPCIACCESS_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/libpciaccess-$(LIBPCIACCESS_VERSION).patch \
-	$(LIBPCIACCESS_PATCH_SITE)/libpciaccess-$(LIBPCIACCESS_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,LIBPCIACCESS)
 
 $(LIBPCIACCESS_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBPCIACCESS_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xzf $(DOWNLOAD_DIR)/$(LIBPCIACCESS_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBPCIACCESS_NEED_PATCH),y)
-	@cd $(PACKAGES_BUILD)/libpciaccess-$(LIBPCIACCESS_VERSION); \
-	patch -p1 < $(DOWNLOAD_DIR)/libpciaccess-$(LIBPCIACCESS_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBPCIACCESS)
 
 $(LIBPCIACCESS_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBPCIACCESS_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBPCIACCESS)
 
 libpciaccess_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libpciaccess...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBPCIACCESS_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBPCIACCESS_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBPCIACCESS_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBPCIACCESS_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBPCIACCESS_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBPCIACCESS_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBPCIACCESS)
