7dee632091d7a0433c172a514cddaab12d18e723
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-21 23:23:52 +0100

    Packages: X11/libpthreadstubs: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libpthreadstubs/libpthreadstubs.mk
+++ packages/x11/libpthreadstubs/libpthreadstubs.mk
@@ -2,18 +2,19 @@
 # Embtoolkit
 # Copyright(C) 2010-2011 Abdoulaye Walsimou GAYE. All rights reserved.
 #
-# This program is free software; you can distribute it and/or modify it
-# under the terms of the GNU General Public License
-# (Version 2 or later) published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ################################################################################
 #
 # \file         libpthreadstubs.mk
@@ -22,9 +23,13 @@
 # \date         March 2010
 ################################################################################
 
-LIBPTHREADSTUBS_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBPTHREADSTUBS_VERSION_STRING)))
+LIBPTHREADSTUBS_NAME := libpthread-stubs
+LIBPTHREADSTUBS_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBPTHREADSTUBS)
 LIBPTHREADSTUBS_SITE := http://xcb.freedesktop.org/dist
+LIBPTHREADSTUBS_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+LIBPTHREADSTUBS_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libpthreadstubs/$(LIBPTHREADSTUBS_VERSION)
 LIBPTHREADSTUBS_PACKAGE := libpthread-stubs-$(LIBPTHREADSTUBS_VERSION).tar.bz2
+LIBPTHREADSTUBS_SRC_DIR := $(PACKAGES_BUILD)/libpthread-stubs-$(LIBPTHREADSTUBS_VERSION)
 LIBPTHREADSTUBS_BUILD_DIR := $(PACKAGES_BUILD)/libpthread-stubs-$(LIBPTHREADSTUBS_VERSION)
 
 LIBPTHREADSTUBS_BINS =
@@ -48,46 +53,13 @@ $(LIBPTHREADSTUBS_BUILD_DIR)/.installed: download_libpthreadstubs \
 	@touch $@
 
 download_libpthreadstubs:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBPTHREADSTUBS_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBPTHREADSTUBS_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBPTHREADSTUBS_PACKAGE) \
-	$(LIBPTHREADSTUBS_SITE)/$(LIBPTHREADSTUBS_PACKAGE)
+	$(call EMBTK_DOWNLOAD_PKG,LIBPTHREADSTUBS)
 
 $(LIBPTHREADSTUBS_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBPTHREADSTUBS_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjvf $(DOWNLOAD_DIR)/$(LIBPTHREADSTUBS_PACKAGE)
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBPTHREADSTUBS)
 
 $(LIBPTHREADSTUBS_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBPTHREADSTUBS_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) \
-	--prefix=/usr --libdir=/usr/$(LIBDIR)
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBPTHREADSTUBS)
 
 libpthreadstubs_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libpthreadstubs-$(LIBPTHREADSTUBS_VERSION)...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBPTHREADSTUBS_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBPTHREADSTUBS_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBPTHREADSTUBS_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBPTHREADSTUBS_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBPTHREADSTUBS_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBPTHREADSTUBS_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBPTHREADSTUBS)
