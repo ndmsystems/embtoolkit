a0004e9ab84e626e90b4ab9ab65a6fc44df411ba
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-22 22:53:07 +0100

    Packages: X11/libXcomposite: converto to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libxcomposite/libxcomposite.mk
+++ packages/x11/libxcomposite/libxcomposite.mk
@@ -23,10 +23,13 @@
 # \date         June 2010
 ################################################################################
 
-LIBXCOMPOSITE_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBXCOMPOSITE_VERSION_STRING)))
+LIBXCOMPOSITE_NAME := libXcomposite
+LIBXCOMPOSITE_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBXCOMPOSITE)
 LIBXCOMPOSITE_SITE := http://xorg.freedesktop.org/archive/individual/lib
 LIBXCOMPOSITE_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libxcomposite/$(LIBXCOMPOSITE_VERSION)
+LIBXCOMPOSITE_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBXCOMPOSITE_PACKAGE := libXcomposite-$(LIBXCOMPOSITE_VERSION).tar.bz2
+LIBXCOMPOSITE_SRC_DIR := $(PACKAGES_BUILD)/libXcomposite-$(LIBXCOMPOSITE_VERSION)
 LIBXCOMPOSITE_BUILD_DIR := $(PACKAGES_BUILD)/libXcomposite-$(LIBXCOMPOSITE_VERSION)
 
 LIBXCOMPOSITE_BINS =
@@ -45,7 +48,6 @@ $(LIBXCOMPOSITE_BUILD_DIR)/.installed: $(LIBXCOMPOSITE_DEPS) download_libxcompos
 	$(LIBXCOMPOSITE_BUILD_DIR)/.decompressed $(LIBXCOMPOSITE_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libxcomposite-$(LIBXCOMPOSITE_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(LIBXCOMPOSITE_BUILD_DIR))
 	$(Q)$(MAKE) -C $(LIBXCOMPOSITE_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(LIBXCOMPOSITE_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -53,55 +55,13 @@ $(LIBXCOMPOSITE_BUILD_DIR)/.installed: $(LIBXCOMPOSITE_DEPS) download_libxcompos
 	@touch $@
 
 download_libxcomposite:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBXCOMPOSITE_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBXCOMPOSITE_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBXCOMPOSITE_PACKAGE) \
-	$(LIBXCOMPOSITE_SITE)/$(LIBXCOMPOSITE_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBXCOMPOSITE_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/libxcomposite-$(LIBXCOMPOSITE_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/libxcomposite-$(LIBXCOMPOSITE_VERSION).patch \
-	$(LIBXCOMPOSITE_PATCH_SITE)/libxcomposite-$(LIBXCOMPOSITE_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,LIBXCOMPOSITE)
 
 $(LIBXCOMPOSITE_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBXCOMPOSITE_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjf $(DOWNLOAD_DIR)/$(LIBXCOMPOSITE_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBXCOMPOSITE_NEED_PATCH),y)
-	@cd $(PACKAGES_BUILD)/libxcomposite-$(LIBXCOMPOSITE_VERSION); \
-	patch -p1 < $(DOWNLOAD_DIR)/libxcomposite-$(LIBXCOMPOSITE_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBXCOMPOSITE)
 
 $(LIBXCOMPOSITE_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBXCOMPOSITE_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBXCOMPOSITE)
 
 libxcomposite_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libxcomposite...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBXCOMPOSITE_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBXCOMPOSITE_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBXCOMPOSITE_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBXCOMPOSITE_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBXCOMPOSITE_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBXCOMPOSITE_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBXCOMPOSITE)
