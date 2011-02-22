723d426067c4a387cbb258a811190e975c39e324
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-22 22:10:41 +0100

    Packages: X11/libXau: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libxau/libxau.mk
+++ packages/x11/libxau/libxau.mk
@@ -23,9 +23,13 @@
 # \date         February 2010
 ################################################################################
 
-LIBXAU_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBXAU_VERSION_STRING)))
+LIBXAU_NAME := libXau
+LIBXAU_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBXAU)
 LIBXAU_SITE := http://xorg.freedesktop.org/archive/individual/lib
+LIBXAU_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+LIBXAU_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libxau/$(LIBXAU_VERSION)
 LIBXAU_PACKAGE := libXau-$(LIBXAU_VERSION).tar.bz2
+LIBXAU_SRC_DIR := $(PACKAGES_BUILD)/libXau-$(LIBXAU_VERSION)
 LIBXAU_BUILD_DIR := $(PACKAGES_BUILD)/libXau-$(LIBXAU_VERSION)
 
 LIBXAU_BINS =
@@ -51,46 +55,13 @@ $(LIBXAU_BUILD_DIR)/.installed: $(LIBXAU_DEPS) download_libxau \
 	@touch $@
 
 download_libxau:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBXAU_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBXAU_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBXAU_PACKAGE) \
-	$(LIBXAU_SITE)/$(LIBXAU_PACKAGE)
+	$(call EMBTK_DOWNLOAD_PKG,LIBXAU)
 
 $(LIBXAU_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBXAU_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjvf $(DOWNLOAD_DIR)/$(LIBXAU_PACKAGE)
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBXAU)
 
 $(LIBXAU_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBXAU_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --prefix=/usr --libdir=/usr/$(LIBDIR) \
-	--disable-malloc0returnsnull
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBXAU)
 
 libxau_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libxau-$(LIBXAU_VERSION)...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBXAU_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBXAU_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBXAU_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBXAU_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBXAU_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBXAU_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBXAU)
