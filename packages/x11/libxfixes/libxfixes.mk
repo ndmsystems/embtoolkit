e04923637e9c5548b8bfa76cc583229b8dd94c7b
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-27 00:45:12 +0100

    Packages: X11/libXfixes: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/libxfixes/libxfixes.mk
+++ packages/x11/libxfixes/libxfixes.mk
@@ -23,10 +23,13 @@
 # \date         June 2010
 ################################################################################
 
+LIBXFIXES_NAME := libXfixes
 LIBXFIXES_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_LIBXFIXES_VERSION_STRING)))
 LIBXFIXES_SITE := http://xorg.freedesktop.org/archive/individual/lib
+LIBXFIXES_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBXFIXES_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libxfixes/$(LIBXFIXES_VERSION)
 LIBXFIXES_PACKAGE := libXfixes-$(LIBXFIXES_VERSION).tar.bz2
+LIBXFIXES_SRC_DIR := $(PACKAGES_BUILD)/libXfixes-$(LIBXFIXES_VERSION)
 LIBXFIXES_BUILD_DIR := $(PACKAGES_BUILD)/libXfixes-$(LIBXFIXES_VERSION)
 
 LIBXFIXES_BINS =
@@ -45,7 +48,6 @@ $(LIBXFIXES_BUILD_DIR)/.installed: $(LIBXFIXES_DEPS) download_libxfixes \
 	$(LIBXFIXES_BUILD_DIR)/.decompressed $(LIBXFIXES_BUILD_DIR)/.configured
 	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
 	libxfixes-$(LIBXFIXES_VERSION) in your root filesystem...")
-	$(call EMBTK_KILL_LT_RPATH,$(LIBXFIXES_BUILD_DIR))
 	$(Q)$(MAKE) -C $(LIBXFIXES_BUILD_DIR) $(J)
 	$(Q)$(MAKE) -C $(LIBXFIXES_BUILD_DIR) DESTDIR=$(SYSROOT) install
 	$(Q)$(MAKE) libtool_files_adapt
@@ -53,55 +55,13 @@ $(LIBXFIXES_BUILD_DIR)/.installed: $(LIBXFIXES_DEPS) download_libxfixes \
 	@touch $@
 
 download_libxfixes:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(LIBXFIXES_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(LIBXFIXES_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(LIBXFIXES_PACKAGE) \
-	$(LIBXFIXES_SITE)/$(LIBXFIXES_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBXFIXES_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/libxfixes-$(LIBXFIXES_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/libxfixes-$(LIBXFIXES_VERSION).patch \
-	$(LIBXFIXES_PATCH_SITE)/libxfixes-$(LIBXFIXES_VERSION)-*.patch
-endif
+	$(call EMBTK_DOWNLOAD_PKG,LIBXFIXES)
 
 $(LIBXFIXES_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(LIBXFIXES_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjf $(DOWNLOAD_DIR)/$(LIBXFIXES_PACKAGE)
-ifeq ($(CONFIG_EMBTK_LIBXFIXES_NEED_PATCH),y)
-	@cd $(PACKAGES_BUILD)/libxfixes-$(LIBXFIXES_VERSION); \
-	patch -p1 < $(DOWNLOAD_DIR)/libxfixes-$(LIBXFIXES_VERSION).patch
-endif
-	@touch $@
+	$(call EMBTK_DECOMPRESS_PKG,LIBXFIXES)
 
 $(LIBXFIXES_BUILD_DIR)/.configured:
-	$(Q)cd $(LIBXFIXES_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_CONFIGURE_PKG,LIBXFIXES)
 
 libxfixes_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup libxfixes...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(LIBXFIXES_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(LIBXFIXES_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(LIBXFIXES_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(LIBXFIXES_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(LIBXFIXES_PKGCONFIGS)
-	$(Q)-rm -rf $(LIBXFIXES_BUILD_DIR)*
-
+	$(call EMBTK_CLEANUP_PKG,LIBXFIXES)
