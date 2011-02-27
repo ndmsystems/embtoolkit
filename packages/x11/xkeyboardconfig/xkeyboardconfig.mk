6b2cf2cc0b7c043c67dc64bf9f83e58f1d6e6f12
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-02-27 23:37:36 +0100

    Packages: X11/xkeyboard-config: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/x11/xkeyboardconfig/xkeyboardconfig.mk
+++ packages/x11/xkeyboardconfig/xkeyboardconfig.mk
@@ -23,9 +23,13 @@
 # \date         March 2010
 ################################################################################
 
-XKEYBOARDCONFIG_VERSION := $(subst ",,$(strip $(CONFIG_EMBTK_XKEYBOARDCONFIG_VERSION_STRING)))
+XKEYBOARDCONFIG_NAME := xkeyboard-config
+XKEYBOARDCONFIG_VERSION := $(call EMBTK_GET_PKG_VERSION,XKEYBOARDCONFIG)
 XKEYBOARDCONFIG_SITE := http://xlibs.freedesktop.org/xkbdesc
+XKEYBOARDCONFIG_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+XKEYBOARDCONFIG_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/xkeyboard-config/$(XKEYBOARDCONFIG_VERSION)
 XKEYBOARDCONFIG_PACKAGE := xkeyboard-config-$(XKEYBOARDCONFIG_VERSION).tar.bz2
+XKEYBOARDCONFIG_SRC_DIR := $(PACKAGES_BUILD)/xkeyboard-config-$(XKEYBOARDCONFIG_VERSION)
 XKEYBOARDCONFIG_BUILD_DIR := $(PACKAGES_BUILD)/xkeyboard-config-$(XKEYBOARDCONFIG_VERSION)
 
 XKEYBOARDCONFIG_BINS =
@@ -37,64 +41,14 @@ XKEYBOARDCONFIG_PKGCONFIGS =
 XKEYBOARDCONFIG_DEPS = xkbcomp_install
 
 xkeyboardconfig_install:
-	@test -e $(XKEYBOARDCONFIG_BUILD_DIR)/.installed || \
-	$(MAKE) $(XKEYBOARDCONFIG_BUILD_DIR)/.installed
+	$(call EMBTK_INSTALL_PKG,XKEYBOARDCONFIG)
 	$(Q)$(MAKE) $(XKEYBOARDCONFIG_BUILD_DIR)/.special
 
-$(XKEYBOARDCONFIG_BUILD_DIR)/.installed: $(XKEYBOARDCONFIG_DEPS) \
-	download_xkeyboardconfig $(XKEYBOARDCONFIG_BUILD_DIR)/.decompressed \
-	$(XKEYBOARDCONFIG_BUILD_DIR)/.configured
-	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
-	xkeyboardconfig-$(XKEYBOARDCONFIG_VERSION) in your root filesystem...")
-	$(Q)$(MAKE) -C $(XKEYBOARDCONFIG_BUILD_DIR) $(J)
-	$(Q)$(MAKE) -C $(XKEYBOARDCONFIG_BUILD_DIR) DESTDIR=$(SYSROOT) install
-	$(Q)$(MAKE) libtool_files_adapt
-	$(Q)$(MAKE) pkgconfig_files_adapt
-	@touch $@
-
 download_xkeyboardconfig:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(XKEYBOARDCONFIG_PACKAGE) \
-	if necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(XKEYBOARDCONFIG_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(XKEYBOARDCONFIG_PACKAGE) \
-	$(XKEYBOARDCONFIG_SITE)/$(XKEYBOARDCONFIG_PACKAGE)
-
-$(XKEYBOARDCONFIG_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(XKEYBOARDCONFIG_PACKAGE) ...")
-	@tar -C $(PACKAGES_BUILD) -xjf $(DOWNLOAD_DIR)/$(XKEYBOARDCONFIG_PACKAGE)
-	@touch $@
-
-$(XKEYBOARDCONFIG_BUILD_DIR)/.configured:
-	$(Q)cd $(XKEYBOARDCONFIG_BUILD_DIR); \
-	CC=$(TARGETCC_CACHED) \
-	CXX=$(TARGETCXX_CACHED) \
-	AR=$(TARGETAR) \
-	RANLIB=$(TARGETRANLIB) \
-	AS=$(CROSS_COMPILE)as \
-	LD=$(TARGETLD) \
-	NM=$(TARGETNM) \
-	STRIP=$(TARGETSTRIP) \
-	OBJDUMP=$(TARGETOBJDUMP) \
-	OBJCOPY=$(TARGETOBJCOPY) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	CXXFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR)" \
-	CPPFLGAS="-I$(SYSROOT)/usr/include" \
-	PKG_CONFIG=$(PKGCONFIG_BIN) \
-	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
-	./configure --build=$(HOST_BUILD) --host=$(STRICT_GNU_TARGET) \
-	--target=$(STRICT_GNU_TARGET) --libdir=/usr/$(LIBDIR) \
-	--prefix=/usr
-	@touch $@
+	$(call EMBTK_DOWNLOAD_PKG,XKEYBOARDCONFIG)
 
 xkeyboardconfig_clean:
-	$(call EMBTK_GENERIC_MESSAGE,"cleanup xkeyboardconfig...")
-	$(Q)-cd $(SYSROOT)/usr/bin; rm -rf $(XKEYBOARDCONFIG_BINS)
-	$(Q)-cd $(SYSROOT)/usr/sbin; rm -rf $(XKEYBOARDCONFIG_SBINS)
-	$(Q)-cd $(SYSROOT)/usr/include; rm -rf $(XKEYBOARDCONFIG_INCLUDES)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR); rm -rf $(XKEYBOARDCONFIG_LIBS)
-	$(Q)-cd $(SYSROOT)/usr/$(LIBDIR)/pkgconfig; rm -rf $(XKEYBOARDCONFIG_PKGCONFIGS)
-	$(Q)-rm -rf $(XKEYBOARDCONFIG_BUILD_DIR)*
+	$(call EMBTK_CLEANUP_PKG,XKEYBOARDCONFIG)
 
 .PHONY: $(XKEYBOARDCONFIG_BUILD_DIR)/.special
 
