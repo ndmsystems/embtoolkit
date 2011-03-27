a395c878b57bc58f0ae8d007a1c6b59bc1d4d84e
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-03-27 16:31:28 +0200

    Host tools: fakeroot: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/fakeroot.mk
+++ mk/fakeroot.mk
@@ -24,44 +24,22 @@
 # \date         June 2009
 ################################################################################
 
-FAKEROOT_VERSION	:= 1.12.4
+FAKEROOT_NAME		:= fakeroot
+FAKEROOT_VERSION	:= $(call EMBTK_GET_PKG_VERSION,FAKEROOT)
 FAKEROOT_SITE		:= http://ftp.debian.org/debian/pool/main/f/fakeroot
+FAKEROOT_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+FAKEROOT_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/fakeroot/$(FAKEROOT_VERSION)
 FAKEROOT_PACKAGE	:= fakeroot_$(FAKEROOT_VERSION).tar.gz
+FAKEROOT_SRC_DIR	:= $(TOOLS_BUILD)/fakeroot-$(FAKEROOT_VERSION)
 FAKEROOT_BUILD_DIR	:= $(TOOLS_BUILD)/fakeroot-build
-FAKEROOT_DIR		:= $(HOSTTOOLS)/usr/local/fakeroot
-FAKEROOT_BIN		:= $(FAKEROOT_DIR)/bin/fakeroot
-FAKEROOT_ENV_FILE	:= $(EMBTK_ROOT)/.fakeroot.001
 
+# fakeroot binaries and env
+FAKEROOT_BIN		:= $(HOSTTOOLS)/usr/bin/fakeroot
+FAKEROOT_ENV_FILE	:= $(EMBTK_ROOT)/.fakeroot.001
 export FAKEROOT_BIN FAKEROOT_ENV_FILE
 
 fakeroot_install:
-	@test -e $(FAKEROOT_BUILD_DIR)/.installed || \
-	$(MAKE) $(FAKEROOT_BUILD_DIR)/.installed
-
-$(FAKEROOT_BUILD_DIR)/.installed: download_fakeroot \
-	$(FAKEROOT_BUILD_DIR)/.decompressed $(FAKEROOT_BUILD_DIR)/.configured
-	@$(MAKE) -C $(FAKEROOT_BUILD_DIR) $(J)
-	$(MAKE) -C $(FAKEROOT_BUILD_DIR) install
-	@touch $@
+	$(call EMBTK_INSTALL_HOSTPKG,FAKEROOT)
 
 download_fakeroot:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(FAKEROOT_PACKAGE) if \
-	necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE) \
-	$(FAKEROOT_SITE)/$(FAKEROOT_PACKAGE)
-
-$(FAKEROOT_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(FAKEROOT_PACKAGE)...")
-	@tar -C $(TOOLS_BUILD) -xzf $(DOWNLOAD_DIR)/$(FAKEROOT_PACKAGE)
-	@mkdir -p $(FAKEROOT_BUILD_DIR)
-	@mkdir -p $(FAKEROOT_DIR)
-	@touch $@
-
-$(FAKEROOT_BUILD_DIR)/.configured:
-	$(call EMBTK_GENERIC_MESSAGE,"Configuring \
-	fakeroot_$(FAKEROOT_VERSION)...")
-	@cd $(FAKEROOT_BUILD_DIR); \
-	$(TOOLS_BUILD)/fakeroot-$(FAKEROOT_VERSION)/configure \
-	--prefix=$(FAKEROOT_DIR) --build=$(HOST_BUILD) --host=$(HOST_ARCH)
-	@touch $@
+	$(call EMBTK_DOWNLOAD_PKG,FAKEROOT)
