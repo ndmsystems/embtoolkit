dd57bb704f33538b3939a559aa743abe6f1d7c66
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-03-27 15:01:27 +0200

    Host tools: m4: convert to use install macro
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/m4.mk
+++ mk/m4.mk
@@ -23,38 +23,23 @@
 # \date         February 2010
 ################################################################################
 
-M4_NAME := m4
-M4_VERSION := $(call EMBTK_GET_PKG_VERSION,M4)
-M4_SITE := http://ftp.gnu.org/gnu/m4
-M4_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
-M4_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/m4/$(M4_VERSION)
-M4_PACKAGE := m4-$(M4_VERSION).tar.bz2
-M4_SRC_DIR := $(TOOLS_BUILD)/m4-$(M4_VERSION)
-M4_BUILD_DIR := $(TOOLS_BUILD)/m4-$(M4_VERSION)
+M4_NAME		:= m4
+M4_VERSION	:= $(call EMBTK_GET_PKG_VERSION,M4)
+M4_SITE		:= http://ftp.gnu.org/gnu/m4
+M4_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+M4_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/m4/$(M4_VERSION)
+M4_PACKAGE	:= m4-$(M4_VERSION).tar.bz2
+M4_SRC_DIR	:= $(TOOLS_BUILD)/m4-$(M4_VERSION)
+M4_BUILD_DIR	:= $(TOOLS_BUILD)/m4-$(M4_VERSION)
 
-M4_DIR := $(HOSTTOOLS)/usr
-M4_BIN := $(M4_DIR)/bin/m4
-M4 := $(M4_BIN)
+# m4 binaries
+M4_DIR	:= $(HOSTTOOLS)/usr
+M4_BIN	:= $(M4_DIR)/bin/m4
+M4	:= $(M4_BIN)
 export M4
 
-m4_install: $(M4_BUILD_DIR)/.installed
-
-$(M4_BUILD_DIR)/.installed: download_m4 \
-	$(M4_BUILD_DIR)/.decompressed $(M4_BUILD_DIR)/.configured
-	@$(MAKE) -C $(M4_BUILD_DIR) $(J)
-	$(MAKE) -C $(M4_BUILD_DIR) install
-	@touch $@
+m4_install:
+	$(call EMBTK_INSTALL_HOSTPKG,M4)
 
 download_m4:
 	$(call EMBTK_DOWNLOAD_PKG,M4)
-
-$(M4_BUILD_DIR)/.decompressed:
-	$(call EMBTK_DECOMPRESS_HOSTPKG,M4)
-
-$(M4_BUILD_DIR)/.configured:
-	$(call EMBTK_GENERIC_MESSAGE,"Configuring \
-	m4_$(M4_VERSION)...")
-	@cd $(M4_BUILD_DIR); \
-	$(TOOLS_BUILD)/m4-$(M4_VERSION)/configure \
-	--prefix=$(M4_DIR) --build=$(HOST_BUILD) --host=$(HOST_ARCH)
-	@touch $@
