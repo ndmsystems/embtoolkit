b29b0529a173eaeb06f23db2efcc24f9c83dc154
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-03-27 14:56:41 +0200

    Host tools: automake convert to use install macro
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/automake.mk
+++ mk/automake.mk
@@ -23,40 +23,22 @@
 # \date         February 2010
 ################################################################################
 
-AUTOMAKE_NAME := automake
-AUTOMAKE_VERSION := $(call EMBTK_GET_PKG_VERSION,AUTOMAKE)
-AUTOMAKE_SITE := http://ftp.gnu.org/gnu/automake
-AUTOMAKE_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
-AUTOMAKE_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/automake/$(AUTOMAKE_VERSION)
-AUTOMAKE_PACKAGE := automake-$(AUTOMAKE_VERSION).tar.bz2
-AUTOMAKE_SRC_DIR := $(TOOLS_BUILD)/automake-$(AUTOMAKE_VERSION)
-AUTOMAKE_BUILD_DIR := $(TOOLS_BUILD)/automake-$(AUTOMAKE_VERSION)
+AUTOMAKE_NAME		:= automake
+AUTOMAKE_VERSION	:= $(call EMBTK_GET_PKG_VERSION,AUTOMAKE)
+AUTOMAKE_SITE		:= http://ftp.gnu.org/gnu/automake
+AUTOMAKE_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+AUTOMAKE_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/automake/$(AUTOMAKE_VERSION)
+AUTOMAKE_PACKAGE	:= automake-$(AUTOMAKE_VERSION).tar.bz2
+AUTOMAKE_SRC_DIR	:= $(TOOLS_BUILD)/automake-$(AUTOMAKE_VERSION)
+AUTOMAKE_BUILD_DIR	:= $(TOOLS_BUILD)/automake-$(AUTOMAKE_VERSION)
 
-AUTOMAKE_DIR := $(HOSTTOOLS)/usr
-ACLOCAL := $(AUTOMAKE_DIR)/bin/aclocal
-AUTOMAKE := $(AUTOMAKE_DIR)/bin/automake
-
+AUTOMAKE_DIR	:= $(HOSTTOOLS)/usr
+ACLOCAL		:= $(AUTOMAKE_DIR)/bin/aclocal
+AUTOMAKE	:= $(AUTOMAKE_DIR)/bin/automake
 export ACLOCAL AUTOMAKE
 
-automake_install: $(AUTOMAKE_BUILD_DIR)/.installed
-
-$(AUTOMAKE_BUILD_DIR)/.installed: download_automake \
-	$(AUTOMAKE_BUILD_DIR)/.decompressed $(AUTOMAKE_BUILD_DIR)/.configured
-	@$(MAKE) -C $(AUTOMAKE_BUILD_DIR) $(J)
-	$(MAKE) -C $(AUTOMAKE_BUILD_DIR) install
-	@touch $@
+automake_install:
+	$(call EMBTK_INSTALL_HOSTPKG,AUTOMAKE)
 
 download_automake:
 	$(call EMBTK_DOWNLOAD_PKG,AUTOMAKE)
-
-$(AUTOMAKE_BUILD_DIR)/.decompressed:
-	$(call EMBTK_DECOMPRESS_HOSTPKG,AUTOMAKE)
-
-$(AUTOMAKE_BUILD_DIR)/.configured:
-	$(call EMBTK_GENERIC_MESSAGE,"Configuring \
-	automake_$(AUTOMAKE_VERSION)...")
-	@cd $(AUTOMAKE_BUILD_DIR); \
-	M4=$(M4_BIN) \
-	$(TOOLS_BUILD)/automake-$(AUTOMAKE_VERSION)/configure \
-	--prefix=$(AUTOMAKE_DIR) --build=$(HOST_BUILD) --host=$(HOST_ARCH)
-	@touch $@
