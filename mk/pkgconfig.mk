aeca01d37bb89bb03c2628ec118d3b08cdc49b1a
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-03-27 15:40:44 +0200

    Host tools: pkg-config: convert to use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/pkgconfig.mk
+++ mk/pkgconfig.mk
@@ -1,5 +1,5 @@
 ################################################################################
-# Abdoulaye Walsimou GAYE, <awg@embtoolkit.org>
+# Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
 # Copyright(C) 2009-2011 Abdoulaye Walsimou GAYE.
 #
 # This program is free software: you can redistribute it and/or modify
@@ -19,54 +19,29 @@
 #
 # \file         pkgconfig.mk
 # \brief	pkgconfig.mk of Embtoolkit
-# \author       Abdoulaye Walsimou GAYE, <awg@embtoolkit.org>
+# \author       Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
 # \date         October 2009
 ################################################################################
 
-PKGCONFIG_VERSION := 0.25
-PKGCONFIG_SITE := http://pkgconfig.freedesktop.org/releases
-PKGCONFIG_PACKAGE := pkg-config-$(PKGCONFIG_VERSION).tar.gz
-PKGCONFIG_BUILD_DIR := $(TOOLS_BUILD)/pkg-config-$(PKGCONFIG_VERSION)
-PKGCONFIG_DIR := $(HOSTTOOLS)/usr/local/pkg-config
-PKGCONFIG_BIN := $(PKGCONFIG_DIR)/bin/pkg-config
+PKGCONFIG_NAME		:= pkg-config
+PKGCONFIG_VERSION	:= $(call EMBTK_GET_PKG_VERSION,PKGCONFIG)
+PKGCONFIG_SITE		:= http://pkgconfig.freedesktop.org/releases
+PKGCONFIG_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+PKGCONFIG_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/automake/$(PKGCONFIG_VERSION)
+PKGCONFIG_PACKAGE	:= pkg-config-$(PKGCONFIG_VERSION).tar.gz
+PKGCONFIG_SRC_DIR	:= $(TOOLS_BUILD)/pkg-config-$(PKGCONFIG_VERSION)
+PKGCONFIG_BUILD_DIR	:= $(TOOLS_BUILD)/pkg-config-$(PKGCONFIG_VERSION)
 
-export PKGCONFIG_BIN
-
+PKGCONFIG_BIN	:=  $(HOSTTOOLS)/usr/bin/pkg-config
 ifeq ($(CONFIG_EMBTK_64BITS_FS_COMPAT32),y)
-PKG_CONFIG_PATH=$(SYSROOT)/usr/lib32/pkgconfig
+PKG_CONFIG_PATH	:=$(SYSROOT)/usr/lib32/pkgconfig
 else
-PKG_CONFIG_PATH=$(SYSROOT)/usr/lib/pkgconfig
+PKG_CONFIG_PATH	:=$(SYSROOT)/usr/lib/pkgconfig
 endif
-
-export PKG_CONFIG_PATH
+export PKGCONFIG_BIN PKG_CONFIG_PATH
 
 pkgconfig_install:
-	@test -e $(PKGCONFIG_BUILD_DIR)/.installed || \
-	$(MAKE) $(PKGCONFIG_BUILD_DIR)/.installed
-
-$(PKGCONFIG_BUILD_DIR)/.installed: download_pkgconfig \
-	$(PKGCONFIG_BUILD_DIR)/.decompressed $(PKGCONFIG_BUILD_DIR)/.configured
-	@$(MAKE) -C $(PKGCONFIG_BUILD_DIR) $(J)
-	@$(MAKE) -C $(PKGCONFIG_BUILD_DIR) install
-	@touch $@
+	$(call EMBTK_INSTALL_HOSTPKG,PKGCONFIG)
 
 download_pkgconfig:
-	$(call EMBTK_GENERIC_MESSAGE,"Downloading $(PKGCONFIG_PACKAGE) if \
-	necessary...")
-	@test -e $(DOWNLOAD_DIR)/$(PKGCONFIG_PACKAGE) || \
-	wget -O $(DOWNLOAD_DIR)/$(PKGCONFIG_PACKAGE) \
-	$(PKGCONFIG_SITE)/$(PKGCONFIG_PACKAGE)
-
-$(PKGCONFIG_BUILD_DIR)/.decompressed:
-	$(call EMBTK_GENERIC_MESSAGE,"Decompressing $(PKGCONFIG_PACKAGE)...")
-	@tar -C $(TOOLS_BUILD) -xzf $(DOWNLOAD_DIR)/$(PKGCONFIG_PACKAGE)
-	@touch $@
-
-$(PKGCONFIG_BUILD_DIR)/.configured:
-	$(call EMBTK_GENERIC_MESSAGE,"Configuring \
-	pkg-config-$(PKGCONFIG_VERSION)...")
-	@cd $(PKGCONFIG_BUILD_DIR); \
-	./configure \
-	--prefix=$(PKGCONFIG_DIR) --build=$(HOST_BUILD) --host=$(HOST_ARCH)
-	@touch $@
-
+	$(call EMBTK_DOWNLOAD_PKG,PKGCONFIG)
