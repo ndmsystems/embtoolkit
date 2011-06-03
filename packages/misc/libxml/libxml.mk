f098d0a269e5fbf01afc6116990d00224119e77b
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-06-03 17:51:39 +0200

    Packages: libxml2: convert to fully use generic macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/misc/libxml/libxml.mk
+++ packages/misc/libxml/libxml.mk
@@ -23,45 +23,28 @@
 # \date         December 2009
 ################################################################################
 
-LIBXML2_NAME := libxml2
-LIBXML2_VERSION := $(call EMBTK_GET_PKG_VERSION,LIBXML2)
-LIBXML2_SITE := ftp://xmlsoft.org/libxml2
-LIBXML2_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
-LIBXML2_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/libxml2/$(LIBXML2_VERSION)
-LIBXML2_PACKAGE := libxml2-$(LIBXML2_VERSION).tar.gz
-LIBXML2_SRC_DIR := $(PACKAGES_BUILD)/libxml2-$(LIBXML2_VERSION)
-LIBXML2_BUILD_DIR := $(PACKAGES_BUILD)/libxml2-$(LIBXML2_VERSION)
+LIBXML2_NAME		:= libxml2
+LIBXML2_VERSION		:= $(call EMBTK_GET_PKG_VERSION,LIBXML2)
+LIBXML2_SITE		:= ftp://xmlsoft.org/libxml2
+LIBXML2_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+LIBXML2_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/libxml2/$(LIBXML2_VERSION)
+LIBXML2_PACKAGE		:= libxml2-$(LIBXML2_VERSION).tar.gz
+LIBXML2_SRC_DIR		:= $(PACKAGES_BUILD)/libxml2-$(LIBXML2_VERSION)
+LIBXML2_BUILD_DIR	:= $(PACKAGES_BUILD)/libxml2-$(LIBXML2_VERSION)
 
-LIBXML2_BINS = xml2-config xmlcatalog xmllint
-LIBXML2_SBINS =
-LIBXML2_INCLUDES = libxml2
-LIBXML2_LIBS = libxml2* xml2Conf.sh
-LIBXML2_PKGCONFIGS = libxml*.pc
+LIBXML2_BINS		= xml2-config xmlcatalog xmllint
+LIBXML2_SBINS		=
+LIBXML2_INCLUDES	= libxml2
+LIBXML2_LIBS		= libxml2* xml2Conf.sh
+LIBXML2_PKGCONFIGS	= libxml*.pc
 
-LIBXML2_CONFIGURE_OPTS := --without-python
+LIBXML2_CONFIGURE_OPTS	:= --without-python
 
 libxml2_install:
-	@test -e $(LIBXML2_BUILD_DIR)/.installed || \
-	$(MAKE) $(LIBXML2_BUILD_DIR)/.installed
-
-$(LIBXML2_BUILD_DIR)/.installed: download_libxml2 \
-	$(LIBXML2_BUILD_DIR)/.decompressed $(LIBXML2_BUILD_DIR)/.configured
-	$(call EMBTK_GENERIC_MESSAGE,"Compiling and installing \
-	libxml2-$(LIBXML2_VERSION) in your root filesystem...")
-	$(Q)$(MAKE) -C $(LIBXML2_BUILD_DIR) $(J)
-	$(Q)$(MAKE) -C $(LIBXML2_BUILD_DIR) DESTDIR=$(SYSROOT) install
-	$(Q)$(MAKE) libtool_files_adapt
-	$(Q)$(MAKE) pkgconfig_files_adapt
-	@touch $@
+	$(call EMBTK_INSTALL_PKG,LIBXML2)
 
 download_libxml2:
 	$(call EMBTK_DOWNLOAD_PKG,LIBXML2)
-
-$(LIBXML2_BUILD_DIR)/.decompressed:
-	$(call EMBTK_DECOMPRESS_PKG,LIBXML2)
-
-$(LIBXML2_BUILD_DIR)/.configured:
-	$(call EMBTK_CONFIGURE_PKG,LIBXML2)
 
 libxml2_clean:
 	$(call EMBTK_CLEANUP_PKG,LIBXML2)
