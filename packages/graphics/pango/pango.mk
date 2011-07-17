2d894ddb836cc4cf9d6a4b7f6fa44378dc7c1a16
Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
2011-07-17 14:44:55 +0200

    Packages: Pango: convert to fully use generic macros
    Signed-off-by: Abdoulaye Walsimou GAYE <awg@embtoolkit.org>

--- packages/graphics/pango/pango.mk
+++ packages/graphics/pango/pango.mk
@@ -23,55 +23,43 @@
 # \date         December 2009
 ################################################################################
 
-PANGO_NAME := pango
-PANGO_MAJOR_VERSION := $(call embtk_get_pkgversion,PANGO_MAJOR)
-PANGO_VERSION := $(call embtk_get_pkgversion,PANGO)
-PANGO_SITE := http://ftp.gnome.org/pub/gnome/sources/pango/$(PANGO_MAJOR_VERSION)
-PANGO_SITE_MIRROR3 := ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
-PANGO_PATCH_SITE := ftp://ftp.embtoolkit.org/embtoolkit.org/pango/$(PANGO_VERSION)
-PANGO_PACKAGE := pango-$(PANGO_VERSION).tar.bz2
-PANGO_SRC_DIR := $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
-PANGO_BUILD_DIR := $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
+PANGO_NAME		:= pango
+PANGO_MAJOR_VERSION	:= $(call embtk_get_pkgversion,pango_major)
+PANGO_VERSION		:= $(call embtk_get_pkgversion,pango)
+PANGO_SITE		:= http://ftp.gnome.org/pub/gnome/sources/pango/$(PANGO_MAJOR_VERSION)
+PANGO_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
+PANGO_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/pango/$(PANGO_VERSION)
+PANGO_PACKAGE		:= pango-$(PANGO_VERSION).tar.bz2
+PANGO_SRC_DIR		:= $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
+PANGO_BUILD_DIR		:= $(PACKAGES_BUILD)/pango-$(PANGO_VERSION)
 
-PANGO_BINS = pango*
-PANGO_SBINS =
-PANGO_INCLUDES = pango*
-PANGO_LIBS = pango-* pango* libpango*
-PANGO_PKGCONFIGS = pango*.pc
+PANGO_BINS		= pango*
+PANGO_SBINS		=
+PANGO_INCLUDES		= pango*
+PANGO_LIBS		= pango-* pango* libpango*
+PANGO_PKGCONFIGS	= pango*.pc
 
-ifeq ($(CONFIG_EMBTK_HAVE_PANGO_WITH_X),y)
-PANGO_CONFIGURE_OPTS := --with-x
-PANGO_DEPS := libx11_install
-else
-PANGO_CONFIGURE_OPTS := --without-x
-endif
 
-PANGO_DEPS += glib_install fontconfig_install cairo_install
+PANGO_CONFIGURE_OPTS-y	:= $(if $(CONFIG_EMBTK_HAVE_PANGO_WITH_X),		\
+				--with-x,--without-x)
+PANGO_DEPS-y		:= $(if $(CONFIG_EMBTK_HAVE_PANGO_WITH_X),		\
+				libx11_install)
+
+PANGO_CONFIGURE_OPTS	:= $(PANGO_CONFIGURE_OPTS-y)
+PANGO_DEPS		:= glib_install fontconfig_install $(PANGO_DEPS-y)	\
+			cairo_install
 
 pango_install:
-	@test -e $(PANGO_BUILD_DIR)/.installed || \
-	$(MAKE) $(PANGO_BUILD_DIR)/.installed
+	$(call embtk_install_pkg,pango)
+	$(Q)test -e $(PANGO_BUILD_DIR)/.patchlibtool || \
+	$(MAKE) $(PANGO_BUILD_DIR)/.patchlibtool
 	$(Q)$(MAKE) $(PANGO_BUILD_DIR)/.special
 
-$(PANGO_BUILD_DIR)/.installed: $(PANGO_DEPS) download_pango \
-	$(PANGO_BUILD_DIR)/.decompressed $(PANGO_BUILD_DIR)/.configured
-	$(call embtk_generic_message,"Compiling and installing \
-	pango-$(PANGO_VERSION) in your root filesystem...")
-	$(Q)$(MAKE) -C $(PANGO_BUILD_DIR) $(J)
-	$(Q)$(MAKE) -C $(PANGO_BUILD_DIR) DESTDIR=$(SYSROOT) install
-	$(Q)$(MAKE) libtool_files_adapt
-	$(Q)$(MAKE) pkgconfig_files_adapt
-	$(Q)$(MAKE) $(PANGO_BUILD_DIR)/.patchlibtool
-	@touch $@
-
 download_pango:
-	$(call embtk_download_pkg,PANGO)
+	$(call embtk_download_pkg,pango)
 
-$(PANGO_BUILD_DIR)/.decompressed:
-	$(call embtk_decompress_pkg,PANGO)
-
-$(PANGO_BUILD_DIR)/.configured:
-	$(call embtk_configure_pkg,PANGO)
+pango_clean:
+	$(call embtk_cleanup_pkg,pango)
 
 $(PANGO_BUILD_DIR)/.patchlibtool:
 	$(Q)PANGO_LT_FILES=`find $(SYSROOT)/usr/$(LIBDIR)/* -type f -name *.la`; \
@@ -83,12 +71,10 @@ $(PANGO_BUILD_DIR)/.patchlibtool:
 	< $$i > $$i.new; \
 	mv $$i.new $$i; \
 	done
+	@touch $@
 
 .PHONY: pango_clean $(PANGO_BUILD_DIR)/.special
 
 $(PANGO_BUILD_DIR)/.special:
 	$(Q)-cp -R $(SYSROOT)/usr/$(LIBDIR)/pango $(ROOTFS)/usr/$(LIBDIR)/
 	@touch $@
-
-pango_clean:
-	$(call embtk_cleanup_pkg,PANGO)
