90540a79d7e23f5bb04b57d9e3b102fc29cd0495
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-08-29 21:44:42 +0200

    Toolchain: uClibc: use generic download and decompress macros
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/uclibc.mk
+++ mk/uclibc.mk
@@ -23,13 +23,14 @@
 # \date         August 2009
 ################################################################################
 
-UCLIBC_VERSION		:= $(call embtk_get_pkgversion,uclibc)
+UCLIBC_NAME		:= uClibc
+UCLIBC_VERSION		:= $(call embtk_get_pkgversion,uClibc)
 UCLIBC_SITE		:= http://www.uclibc.org/downloads
-UCLIBC_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/uclibc/$(UCLIBC_VERSION)
 UCLIBC_GIT_SITE		:= http://git.busybox.net/uClibc
 UCLIBC_GIT_BRANCH	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_GIT_BRANCH)))
 UCLIBC_GIT_REVISION	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_GIT_REVISION)))
 UCLIBC_PACKAGE		:= uClibc-$(UCLIBC_VERSION).tar.bz2
+UCLIBC_SRC_DIR		:= $(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)
 UCLIBC_BUILD_DIR	:= $(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)
 
 UCLIBC_DOTCONFIG	:= $(UCLIBC_BUILD_DIR)/.config
@@ -39,6 +40,7 @@ EMBTK_UCLIBC_CFLAGS	+= $(EMBTK_TARGET_ABI) $(EMBTK_TARGET_FLOAT_CFLAGS)
 EMBTK_UCLIBC_CFLAGS	+= $(EMBTK_TARGET_MARCH) -pipe
 
 uclibc_install: $(UCLIBC_BUILD_DIR)/.installed
+
 uclibc_headers_install: $(UCLIBC_BUILD_DIR)/.headers_installed
 
 $(UCLIBC_BUILD_DIR)/.installed:
@@ -50,8 +52,9 @@ $(UCLIBC_BUILD_DIR)/.installed:
 		RUNTIME_PREFIX="/" DEVEL_PREFIX="/usr/"				\
 		KERNEL_HEADERS="$(SYSROOT)/usr/include/"			\
 		UCLIBC_EXTRA_CFLAGS="$(EMBTK_UCLIBC_CFLAGS)" install
+	$(Q)touch $@
 
-$(UCLIBC_BUILD_DIR)/.headers_installed: uclibc_download \
+$(UCLIBC_BUILD_DIR)/.headers_installed: download_uclibc \
 	$(UCLIBC_BUILD_DIR)/.decompressed $(UCLIBC_BUILD_DIR)/.configured
 	$(call embtk_generic_msg,"Install uClibc-$(UCLIBC_VERSION) headers ...")
 	$(Q)$(MAKE) -C $(UCLIBC_BUILD_DIR) silentoldconfig
@@ -73,10 +76,9 @@ $(UCLIBC_BUILD_DIR)/.headers_installed: uclibc_download \
 	-shared -x c /dev/null -o $(SYSROOT)/usr/$(LIBDIR)/libc.so
 	$(Q)touch $@
 
-uclibc_download:
-	$(call embtk_generic_msg,"downloading uClibc-$(UCLIBC_VERSION) \
-	if necessary ...")
+download_uclibc download_uclibc_headers:
 ifeq ($(CONFIG_EMBTK_UCLIBC_VERSION_GIT),y)
+	$(call embtk_generic_msg,"downloading uClibc using GIT")
 	@test -e $(EMBTK_ROOT)/src/uClibc-git || \
 	git clone $(UCLIBC_GIT_SITE) $(EMBTK_ROOT)/src/uClibc-git
 ifneq ($(UCLIBC_GIT_BRANCH),master)
@@ -91,24 +93,11 @@ endif
 	--prefix=uClibc-$(UCLIBC_VERSION)/ $(UCLIBC_GIT_REVISION) | \
 	bzip2 -9 > $(DOWNLOAD_DIR)/$(UCLIBC_PACKAGE)
 else
-	@test -e $(DOWNLOAD_DIR)/$(UCLIBC_PACKAGE) || \
-	wget $(UCLIBC_SITE)/$(UCLIBC_PACKAGE) \
-	-O $(DOWNLOAD_DIR)/$(UCLIBC_PACKAGE)
-endif
-ifeq ($(CONFIG_EMBTK_UCLIBC_NEED_PATCH),y)
-	@test -e $(DOWNLOAD_DIR)/uClibc-$(UCLIBC_VERSION).patch || \
-	wget -O $(DOWNLOAD_DIR)/uClibc-$(UCLIBC_VERSION).patch \
-	$(UCLIBC_PATCH_SITE)/uClibc-$(UCLIBC_VERSION)-*.patch
+	$(call embtk_download_pkg,uClibc)
 endif
 
 $(UCLIBC_BUILD_DIR)/.decompressed:
-	$(call embtk_generic_msg,"Decompressing \
-	uClibc-$(UCLIBC_VERSION) ...")
-	$(Q)tar -C $(TOOLS_BUILD) -xjvf $(DOWNLOAD_DIR)/$(UCLIBC_PACKAGE)
-ifeq ($(CONFIG_EMBTK_UCLIBC_NEED_PATCH),y)
-	$(Q)cd $(UCLIBC_BUILD_DIR); \
-	patch -p1 < $(DOWNLOAD_DIR)/uClibc-$(UCLIBC_VERSION).patch
-endif
+	$(call embtk_decompress_hostpkg,uClibc)
 
 #
 # uClibc configuration macros and target
