7bc9c4634986cf099ecd7f83c942d90e6aa6d7ec
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-08-30 01:53:02 +0200

    Move embtk help in its own makefile
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 ################################################################################
-# Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
-# Copyright(C) 2009-2010 Abdoulaye Walsimou GAYE.
+# EmbToolkit
+# Copyright(C) 2009-2011 Abdoulaye Walsimou GAYE.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -151,41 +151,7 @@ include mk/initialpath.mk
 include mk/toolchain.mk
 include mk/packages.mk
 include mk/rootfs.mk
-
-help:
-	$(call EMBTK_GENERIC_MESSAGE,"Embedded systems Toolkit help. Please \
-	visit - http://embtoolkit.org -for more details")
-	@echo " ---------------------------"
-	@echo "| Building and configuring: |"
-	@echo " ---------------------------"
-	@echo "make xconfig:    Show EmbToolkit configure GUI and let you to"
-	@echo "                 configure your toolchain and your root"
-	@echo "                 filesystem (if selected)."
-	@echo
-	@echo "make menuconfig: Same as xconfig but using this time ncurse GUI."
-	@echo
-	@echo "make:            Start building your toolchain and your root"
-	@echo "                 filesystem (if selected) or start xconfig if"
-	@echo "                 you did not configure before."
-	@echo
-	@echo " -----------"
-	@echo "| Cleaning: |"
-	@echo " -----------"
-	@echo "make clean:      Remove all built files, but keep downloaded"
-	@echo "                 packages and host tools."
-	@echo
-	@echo "make distclean:  Same as clean, but remove all downloaded"
-	@echo "                 packages, host tools and .config.old files."
-	@echo
-	@echo " -----------------"
-	@echo "| Root filesystem |"
-	@echo " -----------------"
-	@echo "make rootfs_build:"
-	@echo "                 if after a first build of your toolchain and"
-	@echo "                 your root filesystem, you change the contents"
-	@echo "                 of the root filesystem, use this target to"
-	@echo "                 rebuild it."
-	@echo
+include mk/help.mk
 
 distclean: clean
 	$(Q)rm -rf dl/* src/eglibc* host-tools* .config.old
---
+++ mk/help.mk
@@ -0,0 +1,59 @@
+################################################################################
+# EmbToolkit
+# Copyright(C) 2011 Abdoulaye Walsimou GAYE.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+#
+# \file         help.mk
+# \brief	help.mk of Embtoolkit
+# \author       Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
+# \date         August 2011
+################################################################################
+
+help:
+	$(call embtk_generic_msg,"Embedded systems Toolkit help. Please \
+	visit - http://embtoolkit.org - for more details")
+	@echo " ---------------------------"
+	@echo "| Building and configuring: |"
+	@echo " ---------------------------"
+	@echo "make xconfig:    Show EmbToolkit configure GUI and let you to"
+	@echo "                 configure your toolchain and your root"
+	@echo "                 filesystem (if selected)."
+	@echo
+	@echo "make menuconfig: Same as xconfig but using this time ncurse GUI."
+	@echo
+	@echo "make:            Start building your toolchain and your root"
+	@echo "                 filesystem (if selected) or start xconfig if"
+	@echo "                 you did not configure before."
+	@echo
+	@echo " -----------"
+	@echo "| Cleaning: |"
+	@echo " -----------"
+	@echo "make clean:      Remove all built files, but keep downloaded"
+	@echo "                 packages."
+	@echo
+	@echo "make distclean:  Same as clean, but remove all downloaded"
+	@echo "                 packages and .config.old files."
+	@echo
+	@echo " -----------------"
+	@echo "| Root filesystem |"
+	@echo " -----------------"
+	@echo "make rootfs_build:"
+	@echo "                 if after a first build of your toolchain and"
+	@echo "                 your root filesystem, you change the contents"
+	@echo "                 of the root filesystem, use this target to"
+	@echo "                 rebuild it."
+	@echo
