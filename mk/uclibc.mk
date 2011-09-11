598696b67e67ec2a23018a20dbdf1a62d102364a
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-09-11 13:28:24 +0200

    Toolchain: uClibc: use generic macros to download from git
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/uclibc.mk
+++ mk/uclibc.mk
@@ -23,15 +23,17 @@
 # \date         August 2009
 ################################################################################
 
+__uclibc_refspec	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_REFSPEC)))
+
 UCLIBC_NAME		:= uClibc
 UCLIBC_VERSION		:= $(call embtk_get_pkgversion,uClibc)
 UCLIBC_SITE		:= http://www.uclibc.org/downloads
-UCLIBC_GIT_SITE		:= http://git.busybox.net/uClibc
-UCLIBC_GIT_BRANCH	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_GIT_BRANCH)))
-UCLIBC_GIT_REVISION	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_GIT_REVISION)))
+UCLIBC_GIT_SITE		:= git://git.busybox.net/uClibc
 UCLIBC_PACKAGE		:= uClibc-$(UCLIBC_VERSION).tar.bz2
 UCLIBC_SRC_DIR		:= $(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)
-UCLIBC_BUILD_DIR	:= $(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)
+UCLIBC_BUILD_DIR	:= $(strip $(if $(CONFIG_EMBTK_UCLIBC_VERSION_GIT),	\
+		$(EMBTK_ROOT)/src/$(__uclibc_refspec)/$(UCLIBC_NAME).git, 	\
+		$(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)))
 
 UCLIBC_DOTCONFIG	:= $(UCLIBC_BUILD_DIR)/.config
 
@@ -74,29 +76,12 @@ $(UCLIBC_BUILD_DIR)/.headers_installed: download_uclibc \
 		RUNTIME_PREFIX="/" DEVEL_PREFIX="/usr/"				\
 		KERNEL_HEADERS="$(SYSROOT)/usr/include/"			\
 		UCLIBC_EXTRA_CFLAGS="$(EMBTK_UCLIBC_CFLAGS)" install_startfiles
-	$(Q)$(TOOLS)/bin/$(STRICT_GNU_TARGET)-gcc -nostdlib -nostartfiles 	\
-	-shared -x c /dev/null -o $(SYSROOT)/usr/$(LIBDIR)/libc.so
+	$(Q)$(TARGETCC) -nostdlib -nostartfiles -shared -x c /dev/null		\
+					-o $(SYSROOT)/usr/$(LIBDIR)/libc.so
 	$(Q)touch $@
 
-download_uclibc download_uclibc_headers:
-ifeq ($(CONFIG_EMBTK_UCLIBC_VERSION_GIT),y)
-	$(call embtk_pinfo,"downloading uClibc using GIT")
-	@test -e $(EMBTK_ROOT)/src/uClibc-git || \
-	git clone $(UCLIBC_GIT_SITE) $(EMBTK_ROOT)/src/uClibc-git
-ifneq ($(UCLIBC_GIT_BRANCH),master)
-	@-cd $(EMBTK_ROOT)/src/uClibc-git; \
-	git checkout master; \
-	git pull; \
-	git branch -D $(UCLIBC_GIT_BRANCH); \
-	git checkout -b $(UCLIBC_GIT_BRANCH) origin/$(UCLIBC_GIT_BRANCH)
-endif
-	@cd $(EMBTK_ROOT)/src/uClibc-git; \
-	git pull; git archive \
-	--prefix=uClibc-$(UCLIBC_VERSION)/ $(UCLIBC_GIT_REVISION) | \
-	bzip2 -9 > $(DOWNLOAD_DIR)/$(UCLIBC_PACKAGE)
-else
+download_uclibc_headers:
 	$(call embtk_download_pkg,uClibc)
-endif
 
 $(UCLIBC_BUILD_DIR)/.decompressed:
 	$(call embtk_decompress_pkg,uClibc)
@@ -109,6 +94,7 @@ __embtk_set_uclibc_config=sed -e 's/CONFIG_KEMBTK_UCLIBC_*//g' > $(UCLIBC_DOTCON
 define embtk_configure_uclibc
 	$(shell  $(__embtk_get_uclibc_config) | $(__embtk_set_uclibc_config))
 endef
+
 $(UCLIBC_BUILD_DIR)/.configured:
-	$(call embtk_pinfo,"Configure uClibc-$(UCLIBC_VERSION) ...")
+	$(call embtk_pinfo,"Configure uClibc-$(UCLIBC_VERSION)...")
 	$(Q)$(call embtk_configure_uclibc)
