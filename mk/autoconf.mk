0565f6182f7aa1b7ea18ae4c67ffd79028d40893
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-10-16 17:31:48 +0200

    Host tools: autoconf: minor autoconf makefile tweak
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/autoconf.mk
+++ mk/autoconf.mk
@@ -24,7 +24,7 @@
 ################################################################################
 
 AUTOCONF_NAME		:= autoconf
-AUTOCONF_VERSION	:= $(call embtk_get_pkgversion,AUTOCONF)
+AUTOCONF_VERSION	:= $(call embtk_get_pkgversion,autoconf)
 AUTOCONF_SITE		:= http://ftp.gnu.org/gnu/autoconf
 AUTOCONF_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 AUTOCONF_PATCH_SITE	:= ftp://ftp.embtoolkit.org/embtoolkit.org/autoconf/$(AUTOCONF_VERSION)
@@ -44,7 +44,7 @@ IFNAMES		:= $(AUTOCONF_DIR)/bin/ifnames
 export AUTOCONF AUTOHEADER AUTOM4TE AUTORECONF AUTOSCAN AUTOUPDATE IFNAMES
 
 autoconf_install:
-	$(call embtk_install_hostpkg,AUTOCONF)
+	$(call embtk_install_hostpkg,autoconf)
 
 download_autoconf:
-	$(call embtk_download_pkg,AUTOCONF)
+	$(call embtk_download_pkg,autoconf)
