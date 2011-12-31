fc47d4271207f221e5b836b3ff1165ef7ea4611a
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-12-31 21:54:01 +0100

    Build system: cosmetic change on successful_build message
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/macros.mk
+++ mk/macros.mk
@@ -106,16 +106,16 @@ embtk_pinfo	= $(call embtk_echo_blue,"$(__embtk_msg_h)\n~~ EmbToolkit ~~ $(1)\n$
 
 # Successful build of EmbToolkit message
 successful_build:
-	$(call embtk_echo_blue," --------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"| Toolchain build log |")
-	$(call embtk_echo_blue," --------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"You successfully build your toolchain for $(GNU_TARGET)")
 	$(call embtk_echo_blue,"Tools built (GCC compiler, Binutils, etc.) are located in:")
 	$(call embtk_echo_blue,"    $(TOOLS)/bin")
 	@echo
-	$(call embtk_echo_blue," ---------------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"| Root file system build log |")
-	$(call embtk_echo_blue," ---------------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
 ifeq ($(CONFIG_EMBTK_HAVE_ROOTFS),y)
 	$(call embtk_echo_blue,"You also successfully build root filesystem(s) located in the")
 	$(call embtk_echo_blue,"'generated' sub-directory of EmbToolkit.")
@@ -123,9 +123,9 @@ else
 	$(call embtk_echo_green,"Build of root filesystem not selected.")
 endif
 	@echo
-	$(call embtk_echo_blue," ---------------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"| Embedded systems Toolkit   |")
-	$(call embtk_echo_blue," ---------------------------- ")
+	$(call embtk_echo_blue," ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"Hope that EmbToolkit will be useful for your project !!!")
 	$(call embtk_echo_blue,"Please report any bugs/suggestion at:")
 	$(call embtk_echo_blue,"   http://www.embtoolkit.org/issues/projects/show/embtoolkit")
