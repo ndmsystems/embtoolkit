3bc1e78884c0e63854fd8c6990813c65818555a2
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2011-12-31 23:37:55 +0100

    Toolchain: uClibc: improve installation
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/uclibc.mk
+++ mk/uclibc.mk
@@ -23,17 +23,13 @@
 # \date         August 2009
 ################################################################################
 
-__uclibc_refspec	:= $(subst ",,$(strip $(CONFIG_EMBTK_UCLIBC_REFSPEC)))
-
 UCLIBC_NAME		:= uClibc
 UCLIBC_VERSION		:= $(call embtk_get_pkgversion,uClibc)
 UCLIBC_SITE		:= http://www.uclibc.org/downloads
 UCLIBC_GIT_SITE		:= git://git.busybox.net/uClibc
 UCLIBC_PACKAGE		:= uClibc-$(UCLIBC_VERSION).tar.bz2
 UCLIBC_SRC_DIR		:= $(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)
-UCLIBC_BUILD_DIR	:= $(strip $(if $(CONFIG_EMBTK_UCLIBC_VERSION_GIT),	\
-		$(EMBTK_ROOT)/src/$(__uclibc_refspec)/$(UCLIBC_NAME).git, 	\
-		$(TOOLS_BUILD)/uClibc-$(UCLIBC_VERSION)))
+UCLIBC_BUILD_DIR	:= $(call __embtk_pkg_srcdir,uClibc)
 
 UCLIBC_DOTCONFIG	:= $(UCLIBC_BUILD_DIR)/.config
 
@@ -41,11 +37,13 @@ EMBTK_UCLIBC_CFLAGS	:= $(TARGET_CFLAGS) $(EMBTK_TARGET_MCPU)
 EMBTK_UCLIBC_CFLAGS	+= $(EMBTK_TARGET_ABI) $(EMBTK_TARGET_FLOAT_CFLAGS)
 EMBTK_UCLIBC_CFLAGS	+= $(EMBTK_TARGET_MARCH) -pipe
 
-uclibc_install: $(UCLIBC_BUILD_DIR)/.installed
-	$(call embtk_pinfo,"Successfully installed uClibc")
+uclibc_install:
+	$(Q)test -e $(UCLIBC_BUILD_DIR)/.installed ||				\
+				$(MAKE) $(UCLIBC_BUILD_DIR)/.installed
 
-uclibc_headers_install: $(UCLIBC_BUILD_DIR)/.headers_installed
-	$(call embtk_pinfo,"Successfully installed uClibc headers")
+uclibc_headers_install:
+	$(Q)test -e $(UCLIBC_BUILD_DIR)/.headers_installed ||			\
+				$(MAKE) $(UCLIBC_BUILD_DIR)/.headers_installed
 
 $(UCLIBC_BUILD_DIR)/.installed:
 	$(call embtk_pinfo,"Build and install uClibc-$(UCLIBC_VERSION) ...")
@@ -56,10 +54,12 @@ $(UCLIBC_BUILD_DIR)/.installed:
 		RUNTIME_PREFIX="/" DEVEL_PREFIX="/usr/"				\
 		KERNEL_HEADERS="$(SYSROOT)/usr/include/"			\
 		UCLIBC_EXTRA_CFLAGS="$(EMBTK_UCLIBC_CFLAGS)" install
+	$(Q)touch $@
 
-$(UCLIBC_BUILD_DIR)/.headers_installed: download_uclibc \
-	$(UCLIBC_BUILD_DIR)/.decompressed
+$(UCLIBC_BUILD_DIR)/.headers_installed:
 	$(call embtk_pinfo,"Install uClibc-$(UCLIBC_VERSION) headers ...")
+	$(call embtk_download_pkg,uClibc)
+	$(call embtk_decompress_pkg,uClibc)
 	$(Q)$(MAKE) -C $(UCLIBC_BUILD_DIR) distclean
 	$(Q)$(embtk_configure_uclibc)
 	$(Q)$(MAKE) -C $(UCLIBC_BUILD_DIR) silentoldconfig
@@ -79,12 +79,10 @@ $(UCLIBC_BUILD_DIR)/.headers_installed: download_uclibc \
 		UCLIBC_EXTRA_CFLAGS="$(EMBTK_UCLIBC_CFLAGS)" install_startfiles
 	$(Q)$(TARGETCC) -nostdlib -nostartfiles -shared -x c /dev/null		\
 					-o $(SYSROOT)/usr/$(LIBDIR)/libc.so
+	$(Q)touch $@
 
 download_uclibc_headers:
 	$(call embtk_download_pkg,uClibc)
-
-$(UCLIBC_BUILD_DIR)/.decompressed:
-	$(call embtk_decompress_pkg,uClibc)
 
 #
 # uClibc configuration macros and target
