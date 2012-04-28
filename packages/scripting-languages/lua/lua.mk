ca32fba11eafb4ca12ec885efb4b7097709575c6
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2012-04-28 13:18:25 +0200

    Packges: lua: compile with -fPIC compiler switch
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/scripting-languages/lua/lua.mk
+++ packages/scripting-languages/lua/lua.mk
@@ -47,11 +47,10 @@ lua_install:
 	$(call embtk_makeinstall_pkg,lua)
 
 define embtk_beforeinstall_lua
-	$(Q)$(MAKE) -C $(LUA_BUILD_DIR) CC=$(TARGETCC_CACHED)			\
+	$(Q)$(MAKE) -C $(LUA_BUILD_DIR) PLAT=ansi CC=$(TARGETCC_CACHED)		\
 	AR="$(TARGETAR) rcu" RANLIB=$(TARGETRANLIB)				\
 	LDFLAGS="-L$(SYSROOT)/$(LIBDIR) -L$(SYSROOT)/usr/$(LIBDIR) -lm -ldl"	\
-	CFLAGS="$(TARGET_CFLAGS) -I$(SYSROOT)/usr/include $(LUACONF_H_OPTS)"	\
-	PLAT=ansi
+	CFLAGS="-fPIC $(TARGET_CFLAGS) -I$(SYSROOT)/usr/include $(LUACONF_H_OPTS)"
 endef
 
 define embtk_postinstall_lua
