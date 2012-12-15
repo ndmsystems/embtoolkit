c0c7550f2b88a52399f47a859767142655e38766
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2012-12-15 16:36:20 +0100

    Tollchain: ccache: create new variables for clang and gcc
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/ccache.mk
+++ mk/ccache.mk
@@ -42,14 +42,28 @@ CCACHE_BIN		:= $(CCACHE_HOST_DIR)/bin/ccache
 # Variables for use in env
 HOSTCC_CACHED		:= "$(CCACHE_BIN) $(HOSTCC)"
 HOSTCXX_CACHED		:= "$(CCACHE_BIN) $(HOSTCXX)"
+
 TARGETCC_CACHED		:= "$(CCACHE_BIN) $(TARGETCC)"
 TARGETCXX_CACHED	:= "$(CCACHE_BIN) $(TARGETCXX)"
+
+TARGETGCC_CACHED	:= "$(CCACHE_BIN) $(TARGETGCC)"
+TARGETGCXX_CACHED	:= "$(CCACHE_BIN) $(TARGETGCXX)"
+
+TARGETCLANG_CACHED	:= "$(CCACHE_BIN) $(TARGETCLANG)"
+TARGETCLANGXX_CACHED	:= "$(CCACHE_BIN) $(TARGETCLANGXX)"
 
 # Variables for use directly
 hostcc_cached		:= $(CCACHE_BIN) $(HOSTCC)
 hostcxx_cached		:= $(CCACHE_BIN) $(HOSTCXX)
+
 targetcc_cached		:= $(CCACHE_BIN) $(TARGETCC)
 targetcxx_cached	:= $(CCACHE_BIN) $(TARGETCXX)
+
+targetgcc_cached	:= $(CCACHE_BIN) $(TARGETGCC)
+targetgcxx_cached	:= $(CCACHE_BIN) $(TARGETGCXX)
+
+targetclang_cached	:= $(CCACHE_BIN) $(TARGETCLANG)
+targetclangxx_cached	:= $(CCACHE_BIN) $(TARGETCLANGXX)
 
 export CCACHE_DIR HOSTCC_CACHED HOSTCXX_CACHED TARGETCC_CACHED TARGETCXX_CACHED
 
