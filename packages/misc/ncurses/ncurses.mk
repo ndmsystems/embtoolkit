3a2f5d3b8726e679dcd869ce935175c259303609
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2013-03-25 11:34:54 +0100

    Packages: ncurses: fix cflags and tweak a little the makefile
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/misc/ncurses/ncurses.mk
+++ packages/misc/ncurses/ncurses.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Embtoolkit
-# Copyright(C) 2010-2011 Abdoulaye Walsimou GAYE.
+# Copyright(C) 2010-2013 Abdoulaye Walsimou GAYE.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,27 +27,27 @@ NCURSES_NAME		:= ncurses
 NCURSES_VERSION		:= $(call embtk_get_pkgversion,ncurses)
 NCURSES_SITE		:= http://ftp.gnu.org/pub/gnu/ncurses
 NCURSES_PACKAGE		:= ncurses-$(NCURSES_VERSION).tar.gz
-NCURSES_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 NCURSES_SRC_DIR		:= $(embtk_pkgb)/ncurses-$(NCURSES_VERSION)
 NCURSES_BUILD_DIR	:= $(embtk_pkgb)/ncurses-$(NCURSES_VERSION)
 
-NCURSES_BINS = captoinfo clear infocmp infotocap ncurses5-config reset tic toe \
-		tput tset
-NCURSES_SBINS =
-NCURSES_INCLUDES = cursesapp.h curses.h cursesp.h cursslk.h etip.h menu.h \
-		ncurses_dll.h panel.h term_entry.h tic.h cursesf.h cursesm.h \
-		cursesw.h eti.h form.h nc_tparm.h ncurses.h termcap.h term.h \
-		unctrl.h
-NCURSES_LIBS = libcurses.a libform.a libform_g.a libmenu.a libmenu_g.a \
-		libncurses.a libncurses++.a libncurses_g.a libpanel.a \
-		libpanel_g.a terminfo
-NCURSES_PKGCONFIGS =
+NCURSES_BINS		:= captoinfo clear infocmp infotocap ncurses5-config
+NCURSES_BINS		+= reset tic toe tput tset
 
-NCURSES_CONFIGURE_ENV	:= CFLAGS="$(CFLAGS) -fPIC" ac_cv_header_locale_h=no
-NCURSES_CONFIGURE_OPTS	:= --disable-rpath --without-cxx-binding	\
-			--without-ada --disable-database		\
-			--enable-termcap --without-progs		\
-			--program-prefix=""
+NCURSES_INCLUDES	:= cursesapp.h curses.h cursesp.h cursslk.h etip.h
+NCURSES_INCLUDES	+= menu.h ncurses_dll.h panel.h term_entry.h tic.h
+NCURSES_INCLUDES	+= cursesf.h cursesm.h cursesw.h eti.h form.h nc_tparm.h
+NCURSES_INCLUDES	+= ncurses.h termcap.h term.h unctrl.h
+
+NCURSES_LIBS		:= libcurses.a libform.a libform_g.a libmenu.a
+NCURSES_LIBS		+= libmenu_g.a libncurses.a libncurses++.a
+NCURSES_LIBS		+= libncurses_g.a libpanel.a libpanel_g.a terminfo
+
+NCURSES_CFLAGS		:= -fPIC
+NCURSES_CONFIGURE_ENV	:= ac_cv_header_locale_h=no
+
+NCURSES_CONFIGURE_OPTS	:= --disable-rpath --without-cxx-binding --without-ada
+NCURSES_CONFIGURE_OPTS	+= --disable-database --enable-termcap --without-progs
+NCURSES_CONFIGURE_OPTS	+= --program-prefix=""
 
 define embtk_postinstall_ncurses
 	$(Q)mkdir -p $(embtk_rootfs)/usr/share
