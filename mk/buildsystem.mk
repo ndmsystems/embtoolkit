f6b69f902f18c6e307113e9a2db8bf284c1a8efe
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2013-04-01 20:17:44 +0200

    Build system: display current version along selected features
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- mk/buildsystem.mk
+++ mk/buildsystem.mk
@@ -95,6 +95,7 @@ define __embtk_mk_print_selectedfeatures
 	$(call embtk_echo_blue," ~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"| Toolchain |")
 	$(call embtk_echo_blue," ~~~~~~~~~~~ ")
+	$(call embtk_echo_blue,"\tEmbToolkit          : v$(EMBTK_VERSION)")
 	$(call embtk_echo_blue,"\tArchitecture        : $(GNU_TARGET_ARCH) ($(EMBTK_MCU_FLAG))")
 	$(call embtk_echo_blue,"\tLinux kernel headers: linux-$(call __embtk_pkg_version,linux)")
 	$(call embtk_echo_blue,"\tBinutils            : binutils-$(call  __embtk_pkg_version,binutils)")
