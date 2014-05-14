9aa8e4cd47bbc709ba1a15886f38e91e8c74935e
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2014-05-14 23:33:39 +0200

    Packages: libxml2: workaround installed xml2Conf.sh
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/misc/libxml/libxml.mk
+++ packages/misc/libxml/libxml.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Embtoolkit
-# Copyright(C) 2009-2011 Abdoulaye Walsimou GAYE.
+# Copyright(C) 2009-2014 Abdoulaye Walsimou GAYE.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,16 +26,21 @@
 LIBXML2_NAME		:= libxml2
 LIBXML2_VERSION		:= $(call embtk_get_pkgversion,libxml2)
 LIBXML2_SITE		:= ftp://xmlsoft.org/libxml2
-LIBXML2_SITE_MIRROR3	:= ftp://ftp.embtoolkit.org/embtoolkit.org/packages-mirror
 LIBXML2_PACKAGE		:= libxml2-$(LIBXML2_VERSION).tar.gz
 LIBXML2_SRC_DIR		:= $(embtk_pkgb)/libxml2-$(LIBXML2_VERSION)
 LIBXML2_BUILD_DIR	:= $(embtk_pkgb)/libxml2-$(LIBXML2_VERSION)
 
-LIBXML2_BINS		= xml2-config xmlcatalog xmllint
-LIBXML2_SBINS		=
-LIBXML2_INCLUDES	= libxml2
-LIBXML2_LIBS		= libxml2* xml2Conf.sh
-LIBXML2_PKGCONFIGS	= libxml*.pc
+LIBXML2_BINS		:= xml2-config xmlcatalog xmllint
+LIBXML2_INCLUDES	:= libxml2
+LIBXML2_LIBS		:= libxml2* xml2Conf.sh
+LIBXML2_PKGCONFIGS	:= libxml*.pc
 
 LIBXML2_CONFIGURE_OPTS	:= --without-python
 
+# FIXME: this should be fixed from upstream libxml2
+pembtk_libxml2conf := $(embtk_sysroot)/usr/$(LIBDIR)/xml2Conf.sh
+define embtk_postinstallonce_libxml2
+	sed -e 's;/usr;$(embtk_sysroot)/usr;g'					\
+		< $(pembtk_libxml2conf) > $(pembtk_libxml2conf).tmp;		\
+	mv $(pembtk_libxml2conf).tmp $(pembtk_libxml2conf)
+endef
