7ed80463f90a8b6886691da1333a1f93ec90e022
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2014-07-14 09:24:46 +0200

    Toolchain: llvm: no need to depend on binutils, it is always installed before
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- core/toolchain/llvm/llvm/llvm.mk
+++ core/toolchain/llvm/llvm/llvm.mk
@@ -32,7 +32,7 @@ LLVM_HOST_PACKAGE	:= llvm-$(LLVM_HOST_VERSION).src.tar.gz
 LLVM_HOST_SRC_DIR	:= $(embtk_toolsb)/llvm-$(LLVM_HOST_VERSION).src
 LLVM_HOST_BUILD_DIR	:= $(embtk_toolsb)/llvm-build
 
-LLVM_HOST_DEPS			:= binutils_install clang_host_install
+LLVM_HOST_DEPS			:= clang_host_install
 
 LLVM_HOST_WITH_HASHSTYLE	:= $(if $(CONFIG_EMBTK_CLIB_UCLIBC),--with-default-hash-style=sysv)
 
