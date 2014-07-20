f46975120528da366e4a43b67d829e0de971a7f4
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2014-07-20 12:12:05 +0200

    Packages/Security: New Dropbear v2014.63
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/security/dropbear/dropbear.mk
+++ packages/security/dropbear/dropbear.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Embtoolkit
-# Copyright(C) 2010-2014 GAYE Abdoulaye Walsimou.
+# Copyright(C) 2014 Abdoulaye Walsimou GAYE.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,14 +17,25 @@
 #
 ################################################################################
 #
-# \file         security.kconfig
-# \brief	security.kconfig of Embtoolkit
-# \author       GAYE Abdoulaye Walsimou, <walsimou@walsimou.com>
-# \date         February 2010
+# \file         dropbear.mk
+# \brief	dropbear.mk of Embtoolkit
+# \author       Abdoulaye Walsimou GAYE <awg@embtoolkit.org>
+# \date         July 2014
 ################################################################################
 
-# Dropbear
-source packages/security/dropbear/dropbear.kconfig
+DROPBEAR_NAME		:= dropbear
+DROPBEAR_VERSION	:= $(call embtk_get_pkgversion,dropbear)
+DROPBEAR_SITE		:= https://matt.ucc.asn.au/dropbear/releases
+DROPBEAR_PACKAGE	:= dropbear-$(DROPBEAR_VERSION).tar.bz2
+DROPBEAR_SRC_DIR	:= $(embtk_pkgb)/dropbear-$(DROPBEAR_VERSION)
+DROPBEAR_BUILD_DIR	:= $(embtk_pkgb)/dropbear-$(DROPBEAR_VERSION)
 
-# LibreSSL
-source packages/security/libressl/libressl.kconfig
+DROPBEAR_BINS		:= dbclient dropbearconvert dropbearkey
+DROPBEAR_SBINS		:= dropbear
+DROPBEAR_SHARES		:= man/man1/dbclient.1 man/man1/dropbearconvert.1
+DROPBEAR_SHARES		+= man/man1/dropbearkey.1
+DROPBEAR_SHARES		+= man/man8/dropbear.8
+
+DROPBEAR_CONFIGURE_OPTS	:= --disable-pam --enable-bundled-libtom
+
+DROPBEAR_DEPS		:= zlib_install
