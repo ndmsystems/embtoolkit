7e75b5703fd4c4b9624bc979aca52d6aee69cefd
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2014-09-17 11:12:10 +0200

    Toolchain deps: update gmp/mpfr/mpc to latest versions available
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- packages/htools/gmp/gmp.mk
+++ packages/htools/gmp/gmp.mk
@@ -26,7 +26,7 @@
 GMP_HOST_NAME		:= gmp
 GMP_HOST_VERSION	:= $(call embtk_get_pkgversion,gmp_host)
 GMP_HOST_SITE		:= ftp://ftp.gmplib.org/pub/gmp-$(GMP_HOST_VERSION)
-GMP_HOST_PACKAGE	:= gmp-$(GMP_HOST_VERSION).tar.bz2
+GMP_HOST_PACKAGE	:= gmp-$(GMP_HOST_VERSION).tar.xz
 GMP_HOST_SRC_DIR	:= $(embtk_toolsb)/gmp-$(GMP_HOST_VERSION)
 GMP_HOST_BUILD_DIR	:= $(embtk_toolsb)/gmp-$(GMP_HOST_VERSION)-build
 
