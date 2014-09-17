92fb5cdcc8b28639f5f280175d14ea023129aa3f
Abdoulaye Walsimou Gaye <awg@embtoolkit.org>
2014-09-17 13:29:11 +0200

    Build system: add floating point info in toolchain summary
    Signed-off-by: Abdoulaye Walsimou Gaye <awg@embtoolkit.org>

--- core/mk/macros.mk
+++ core/mk/macros.mk
@@ -142,6 +142,9 @@ define help_toolchain_summary
 	$(call embtk_echo_blue," ~~~~~~~~~~~ ")
 	$(call embtk_echo_blue,"\tEmbToolkit          : v$(EMBTK_VERSION)")
 	$(call embtk_echo_blue,"\tArchitecture        : $(call embtk_ucase,$(LINUX_ARCH)) ($(EMBTK_MCU_FLAG))")
+	$(call embtk_echo_blue,"\tFloating Point      : $(if $(CONFIG_EMBTK_SOFTFLOAT),Soft,Hard)")
+	$(if $(GCC_WITH_FPU-y),
+	$(call embtk_echo_blue,"\tFPU                 : $(GCC_WITH_FPU-y)"))
 	$(call embtk_echo_blue,"\tLinux kernel headers: linux-$(call __embtk_pkg_version,linux)")
 	$(call embtk_echo_blue,"\tBinutils            : binutils-$(call  __embtk_pkg_version,binutils)")
 	$(if $(CONFIG_EMBTK_HOST_HAVE_LLVM),
