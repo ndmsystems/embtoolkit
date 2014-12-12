7d68ef6cbeae8d8a1a800f869768ca981148d9b1
Eugene Yudin <e.yudin@ndmsystems.com>
2014-12-12 17:22:56 +0300

    Added Node.js toolchain addon.

---
+++ core/toolchain/addons/node/node.mk
@@ -0,0 +1,16 @@
+NODE_HOST_NAME		:= node
+NODE_HOST_VERSION	:= $(call embtk_get_pkgversion,node_host)
+NODE_HOST_SITE		:= http://nodejs.org/dist/v$(NODE_HOST_VERSION)
+NODE_HOST_PACKAGE	:= node-v$(NODE_HOST_VERSION).tar.gz
+NODE_HOST_SRC_DIR	:= $(embtk_toolsb)/node-v$(NODE_HOST_VERSION)
+NODE_HOST_BUILD_DIR	:= $(embtk_toolsb)/node-v$(NODE_HOST_VERSION)
+NODE_HOST_DESTDIR	:= $(embtk_tools)
+
+define embtk_install_node_host
+	$(call embtk_makeinstall_hostpkg,node_host)
+endef
+
+define embtk_beforeinstall_node_host
+	cd $(NODE_HOST_BUILD_DIR); 					\
+	$(CONFIG_SHELL) $(NODE_HOST_SRC_DIR)/configure --prefix=/
+endef
