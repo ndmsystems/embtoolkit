f5bebe3665345c3c7da36486be7e373e3b3d2b70
Eugene Yudin <e.yudin@ndmsystems.com>
2014-12-17 18:54:49 +0300

    Added static Node.js building.

--- core/toolchain/addons/node/node.mk
+++ core/toolchain/addons/node/node.mk
@@ -5,6 +5,7 @@ NODE_HOST_PACKAGE	:= node-v$(NODE_HOST_VERSION).tar.gz
 NODE_HOST_SRC_DIR	:= $(embtk_toolsb)/node-v$(NODE_HOST_VERSION)
 NODE_HOST_BUILD_DIR	:= $(embtk_toolsb)/node-v$(NODE_HOST_VERSION)
 NODE_HOST_DESTDIR	:= $(embtk_tools)
+NODE_HOST_MAKE_OPTS	:= LDFLAGS=-static
 
 define embtk_install_node_host
 	$(call embtk_makeinstall_hostpkg,node_host)
